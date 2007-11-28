be1e34603103524592f2ab0d6ada92f6518c2ba5
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2007-11-28 14:26:42 +0000

    Enable SLES plugin to install PPC.  This includes extending the sles plugin to match os/arch specific templates and making such a template for ppc64.
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@88 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server-2.0/usr/lib/xcat/plugins/sles.pm
+++ xCAT-server-2.0/usr/lib/xcat/plugins/sles.pm
@@ -52,12 +52,25 @@ sub mkinstall {
     my $os = $ent->{os};
     my $arch = $ent->{arch};
     my $profile = $ent->{profile};
-    unless (-r "/usr/share/xcat/install/sles/".$ent->{profile}.".tmpl") {
+    unless (
+        -r "/usr/share/xcat/install/sles/$profile.tmpl" or
+        -r "/usr/share/xcat/install/sles/$profile.$arch.tmpl" or
+        -r "/usr/share/xcat/install/sles/$profile.$os.tmpl" or
+        -r "/usr/share/xcat/install/sles/$profile.$os.$arch.tmpl"
+        ) {
       $callback->({error=>["No AutoYaST template exists for ".$ent->{profile}],errorcode=>[1]});
       next;
     }
     #Call the Template class to do substitution to produce a kickstart file in the autoinst dir
-    xCAT::Template->subvars("/usr/share/xcat/install/sles/".$ent->{profile}.".tmpl","/install/autoinst/".$node,$node);
+    if (-r "/usr/share/xcat/install/sles/$profile.$os.$arch.tmpl") {
+        xCAT::Template->subvars("/usr/share/xcat/install/sles/$profile.$os.$arch.tmpl","/install/autoinst/$node",$node);
+    } elsif (-r "/usr/share/xcat/install/sles/$profile.$arch.tmpl") {
+        xCAT::Template->subvars("/usr/share/xcat/install/sles/$profile.$arch.tmpl","/install/autoinst/$node",$node);
+    } elsif (-r "/usr/share/xcat/install/sles/$profile.$os.tmpl") {
+        xCAT::Template->subvars("/usr/share/xcat/install/sles/$profile.$os.tmpl","/install/autoinst/$node",$node);
+    } elsif (-r "/usr/share/xcat/install/sles/$profile.tmpl") {
+        xCAT::Template->subvars("/usr/share/xcat/install/sles/$profile.tmpl","/install/autoinst/$node",$node);
+    }
     mkpath "/install/postscripts/";
     xCAT::Postage->writescript($node,"/install/postscripts/".$node);
     if (($arch =~ /x86/ and -r "/install/$os/$arch/1/boot/$arch/loader/linux"
