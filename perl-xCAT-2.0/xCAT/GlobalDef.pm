344ffdae95f5df5541ed0d8959d091803c25a181
linggao <linggao@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2007-12-05 22:14:56 +0000

    added GlobalDef.pm for storing global variables for xCAT
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@139 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

---
+++ perl-xCAT-2.0/xCAT/GlobalDef.pm
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+# IBM(c) 2007 EPL license http://www.eclipse.org/legal/epl-v10.html
+package xCAT::GlobalDef;
+
+#--------------------------------------------------------------------------------
+
+=head1    xCAT::GlobalDef
+
+=head2    Package Description
+
+This module contains all the global info for xCAT.
+
+
+=cut
+
+#--------------------------------------------------------------------------------
+
+
+# valid values for nodelist.nodetype column
+$::NODETYPE_OSI="osi"; 
+
+
+
+# valid values for nodelist.status columns or other status 
+$::STATUS_ACTIVE="active";
+$::STATUS_INACTIVE="inactive";
+$::STATUS_UNKNOWN="unknown";
+
+1;
+ 
