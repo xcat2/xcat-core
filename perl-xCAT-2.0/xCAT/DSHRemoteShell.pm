22813dc17c34532c06ddce60599b336e95858957
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2007-12-12 13:40:13 +0000

    Support xdsh api interface
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@165 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

---
+++ perl-xCAT-2.0/xCAT/DSHRemoteShell.pm
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+# IBM(c) 2007 EPL license http://www.eclipse.org/legal/epl-v10.html
+
+package xCAT::DSHRemoteShell;
+
+sub remote_shell_command {
+
+	return undef;
+}
+
+sub remote_copy_command {
+
+	return undef;
+}
+
+1;
