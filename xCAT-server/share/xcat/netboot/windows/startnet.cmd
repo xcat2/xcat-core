fe130f94af7d1cdc891c2f8bc7a2a3bf09a01e00
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2008-10-30 17:29:05 +0000

    -Longer grace period for WindowsPE startup
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@2422 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/share/xcat/netboot/windows/startnet.cmd
+++ xCAT-server/share/xcat/netboot/windows/startnet.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo Initializing, please wait.
 wpeinit
-ping -n 10 127.0.0.1 > NUL 2>&1
+ping -n 60 127.0.0.1 > NUL 2>&1
 md \temp
 for /f "delims=: tokens=2" %%c in ('ipconfig /all ^|find "DHCP Server"') do set XCATD=%%c
 for /f %%c in ('echo %XCATD%') do set XCATD=%%c
