707a73432224f93bce34c1e805dec0caf40fe218
datajerk <datajerk@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2009-03-24 16:55:50 +0000

    Auto GPFS for stateless (and stateful ;-)
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@2966 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

---
+++ xCAT-server/share/xcat/netboot/add-on/autogpfs/autogpfsc.pl
@@ -0,0 +1,131 @@
+#!/usr/bin/perl
+
+use strict;
+use IO::Socket;
+use IO::File;
+use Sys::Syslog qw(:DEFAULT setlogsock);
+
+my $autogpfsdhost = shift;
+my $autogpfsdport = shift;
+my $line;
+
+my $sock = IO::Socket::INET->new(
+	PeerAddr => $autogpfsdhost,
+	PeerPort => $autogpfsdport,
+);
+
+if(!defined($sock)) {
+	logger('info',"could not open socket");
+	exit(2);
+}
+
+#print "Starting autoGPFS...\n";
+
+print $sock "hello\n";
+
+while(<$sock>) {
+	chomp();
+	$line = $_;
+
+	if($line =~ /^busy/) {
+		exit(2);
+	}
+	if($line =~ /^new started/) {
+		next;
+	}
+	if($line =~ /^new ended/) {
+		system("/usr/lpp/mmfs/bin/mmstartup");
+		$line = "";
+		last;
+	}
+	if($line =~ /^new .*/) {
+		s/^new //;
+		$line = $_;
+		last;
+	}
+	if($line =~ /^old started/) {
+		next;
+	}
+	if($line =~ /^old ended/) {
+		system("/usr/lpp/mmfs/bin/mmstartup");
+		$line = "";
+		last;
+	}
+	if($line =~ /^old gpfs /) {
+		my $gotit = 0;
+
+		s/^old gpfs //;
+		$line = $_;
+
+		open(FSTAB,"/etc/fstab");
+		while(<FSTAB>) {
+			chomp();
+			if($_ eq $line) {
+				$gotit = 1;
+				last;
+			}
+		}
+		close(FSTAB);
+
+		if($gotit == 1) {
+			next;
+		}
+
+		open(FSTAB,">>/etc/fstab");
+		print FSTAB "$line\n";
+		close(FSTAB);
+
+		next;
+	}
+	if($line =~ /^old dev /) {
+		my @a = split(/\s+/);
+		my $dev = $a[2];
+		my $major = $a[3];
+		my $minor = $a[4];
+
+		unlink($dev);
+		system("mknod $dev b $major $minor");
+		system("chmod 644 $dev");
+
+		next;
+	}
+	if($line =~ /^old mmsdrfs start/) {
+		unlink("/var/mmfs/gen/mmsdrfs");
+		system("mkdir -p /var/mmfs/gen");
+		open(OUTPUT,">/var/mmfs/gen/mmsdrfs");
+		while(<$sock>) {
+			if($_ =~ /^EOF/) {
+				last;
+			}
+			print OUTPUT $_;
+		}
+		close(OUTPUT);
+		next;
+	}
+	if($line =~ /^old .*/) {
+		s/^old //;
+		$line = $_;
+		last;
+	}
+}
+
+if($line ne "") {
+	logger('err',$line);
+	print "$line\n";
+	exit(1);
+}
+
+#print "autoGPFS complete.\n";
+
+exit(0);
+
+sub logger {
+	my $type = shift;
+	my $msg = shift;
+
+	setlogsock('unix');
+	openlog('autogpfsc','','local0');
+	syslog($type,$msg);
+	closelog();
+}
+
---
+++ xCAT-server/share/xcat/netboot/add-on/autogpfs/autogpfsd.pl
@@ -0,0 +1,364 @@
+#!/usr/bin/perl
+
+use strict;
+use IO::Socket;
+use IO::File;
+use POSIX qw(WNOHANG setsid);
+use Sys::Syslog qw(:DEFAULT setlogsock);
+
+use constant PIDFILE => '/var/lock/autogpfsd/autogpfsd.pid';
+system("mkdir -p /var/lock/autogpfsd");
+
+my $quit = 0;
+my $node;
+my $domain;
+my $port;
+my $max;
+my $mode;
+my $cc=0;
+my $pid=$$;
+my %kids;
+
+open(CONFIG,"/etc/sysconfig/autogpfsd") || \
+	die "cannot open /etc/sysconfig/autogpfsd\n";
+
+while(<CONFIG>) {
+	if(/^domain\b/) {
+		$_ =~ m/^domain\s+(.*)/;
+		$domain = $1;
+	}
+	if(/^autogpfsdport\b/) {
+		$_ =~ m/^autogpfsdport\s+(.*)/;
+		$port = $1;
+	}
+	if(/^autogpfsdmax\b/) {
+		$_ =~ m/^autogpfsdmax\s+(.*)/;
+		$max = $1;
+	}
+	if(/^autogpfsdmode\b/) {
+		$_ =~ m/^autogpfsdmode\s+(.*)/;
+		$mode = $1;
+	}
+}
+
+close(CONFIG);
+
+if($domain eq "") {
+	logger('err',"domain not defined in /etc/sysconfig/autogpfsd");
+	die "domain not defined in /etc/sysconfig/autogpfsd";
+}
+
+if($port eq "") {
+	logger('err',"autogpfsdport not defined in /etc/sysconfig/autogpfsd");
+	die "autogpfsdport not defined in /etc/sysconfig/autogpfsd";
+}
+
+if($max eq "") {
+	logger('err',"autogpfsdmax not defined in /etc/sysconfig/autogpfsd");
+	die "autogpfsdmax not defined in /etc/sysconfig/autogpfsd";
+}
+
+if($mode eq "") {
+	logger('err',"autogpfsdmode not defined in /etc/sysconfig/autogpfsd");
+	die "autogpfsdmode not defined in /etc/sysconfig/autogpfsd";
+}
+
+#$SIG{USR1} = sub {$cc--;};
+$SIG{CHLD} = sub { while ( waitpid(-1,WNOHANG)>0 ) { } };
+$SIG{TERM} = $SIG{INT} = sub { $quit++ };
+
+my $fh = lockfile(PIDFILE);
+my $listen_socket =
+IO::Socket::INET->new(
+	LocalPort => $port,
+	Listen    => 20,
+	Proto     => 'tcp',
+	Reuse     => 1,
+	Timeout   => 60*60,
+);
+die "Cannot create a listening socket: $@" unless $listen_socket;
+
+my $pid = daemonic();
+print $fh $pid;
+close $fh;
+
+logger('info',"waiting for connection");
+
+while (!$quit) {
+	next unless my $connection = $listen_socket->accept;
+
+	my $child = fork();
+
+	if(!defined $child) {
+		logger('err',"cannot fork, exiting");
+		die "cannot fork";
+	}
+
+	if ($child == 0) {
+		$listen_socket->close;
+		$node = gethostbyaddr(inet_aton($connection->peerhost),AF_INET);
+		$node =~ s/\.$domain$//;
+		$node =~ s/-eth\d$//;
+		$node =~ s/-myri\d$//;
+		logger('info',"connection from: $node");
+		interact($connection,$node);
+		#kill(10,$pid);
+		exit 0;
+	}
+
+	$connection->close;
+
+	my $gotmax = 0;
+	do {
+		$cc=0;
+		$kids{$child} = 1;
+		my $key;
+		foreach $key (keys(%kids)) {
+			if(getpgrp($key) != -1) {
+				$cc++;
+			}
+			else {
+				delete $kids{$key};
+			}
+  		}
+		if($cc >= $max) {
+			$gotmax++;
+			sleep(1);
+		}
+		if($gotmax == 1) {
+			$gotmax++;
+			logger('info',"connection count ($cc) max ($max) reached");
+		}
+	} while ($cc >= $max);
+
+	if($gotmax > 0) {
+		logger('info',"connection count ($cc) dropped below max ($max)");
+	}
+}
+
+logger('info',"exiting clean");
+
+sub interact {
+	my $sock = shift;
+	my $node = shift;
+
+	STDIN->fdopen($sock,"<")  or die "Can't reopen STDIN: $!";
+	STDOUT->fdopen($sock,">") or die "Can't reopen STDOUT: $!";
+	STDERR->fdopen($sock,">") or die "Can't reopen STDERR: $!";
+	$| = 1;
+
+#	if($cc > $max) {
+#		logger('info',"rejected connection from: $node ($cc) autogpfsdmax exceeded");
+#		print "busy\n";
+#		return;
+#	}
+
+	while(<>) {
+		my $line = $_;
+
+		if(!hostcheck($node)) {
+			print "unauthorized request\n";
+			logger('info',"unauthorized request from $node");
+			last;
+		}
+
+		if(!open(OUTPUT,"/usr/lpp/mmfs/bin/mmlscluster 2>&1 |")) {
+			print "mmlscluster failed\n";
+			logger('info',"mmlscluster failed");
+			last;
+		}
+
+		my $inlist = 0;
+		my $status = "new";
+		while(<OUTPUT>) {
+			my $line = $_;
+
+			if($line =~ /Node number/ || $line =~ /Daemon node name/) {
+				$line = <OUTPUT>;
+				$inlist = 1;
+				next;
+			}
+
+			if($line =~ /-------/) {
+				$inlist = 1;
+				next;
+			}
+
+			if($inlist == 0) {
+				next;
+			}
+
+			s/^\s+//;
+			my @a = split(/[\.\s]+/);
+			my $lnode = $a[1];
+
+			if($lnode eq $node) {
+				$status = "old";
+				last;
+			}
+		}
+		close(OUTPUT);
+
+		logger('info',"$node detected as $status");
+
+		if($status eq "new") {
+			if($mode ne $status) {
+				print "$status run mmaddnode $node manually\n";
+				logger('info',"run mmaddnode $node manually");
+				last;
+			}
+			my $ok = 0;
+			print "$status started\n";
+			if(!open(OUTPUT,"/usr/lpp/mmfs/bin/mmaddnode $node 2>&1 |")) {
+				print "$status mmaddnode failed\n";
+				logger('info',"mmaddnode $node failed");
+				last;
+			}
+			while(<OUTPUT>) {
+				if(/Command successfully completed/) {
+					$ok = 1;
+				}
+				logger('info',"mmaddnode $node output: $_");
+			}
+			close(OUTPUT);
+			if($ok == 0) {
+				print "$status mmaddnode failed\n";
+			}
+			else {
+				print "$status ended\n";
+			}
+		}
+
+		if($status eq "old") {
+			if($mode ne $status) {
+				print "$status suspending autogpfs while adding nodes\n";
+				logger('info',"suspending autogpfs while adding nodes");
+				last;
+			}
+			my $ok = 0;
+			print "$status started\n";
+
+			if(!open(OUTPUT,"/etc/fstab")) {
+				print "$status failed\n";
+				logger('info',"cannot read /etc/fstab");
+				last;
+			}
+			while(<OUTPUT>) {
+				chomp();
+				s/^\s+//g;
+				if(/^#/) {
+					next;
+				}
+				my @a = split(/\s+/);
+				my $fstype = $a[2];
+				if($fstype ne "gpfs") {
+					next;
+				}
+				print "$status gpfs $_\n";
+
+				s/.*dev=([^,]+).*/\1/;
+				my $devgpfs = "/dev/$_";
+				my $mm = `ls -l $devgpfs | head -1`;
+				$mm =~ s/,//g;
+				@a = split(/\s+/,$mm);
+				my $major = $a[4];
+				my $minor = $a[5];
+
+				print "$status dev $devgpfs $major $minor\n";
+			}
+			close(OUTPUT);
+
+			print "$status mmsdrfs start\n";
+			if(!open(OUTPUT,"/var/mmfs/gen/mmsdrfs")) {
+				print "$status failed\n";
+				logger('info',"cannot read /var/mmfs/gen/mmsdrfs");
+				last;
+			}
+			while(<OUTPUT>) {
+				print $_;
+			}
+			close(OUTPUT);
+			print "EOF\n";
+
+			$ok = 1;
+			
+			if($ok == 0) {
+				print "$status failed\n";
+			}
+			else {
+				print "$status ended\n";
+			}
+		}
+
+		last;
+
+	}
+
+	logger('info',"connection from: $node ended");
+}
+
+sub daemonic {
+	my $child = fork();
+
+	if(!defined $child) {
+		die "Can't fork";
+	}
+	if($child) {
+		exit 0;
+	}
+	setsid();
+	open(STDIN, "</dev/null");
+	open(STDOUT,">/dev/null");
+	open(STDERR,">&STDOUT");
+	chdir('/');
+	umask(0);
+	return $$;
+}
+
+sub lockfile {
+	my $file = shift;
+	my $fh;
+
+	if(-e $file) {
+		if(!($fh = IO::File->new($file))) {
+			return($fh);
+		}
+		my $pid = <$fh>;
+		if(kill 0 => $pid) {
+			die "Server already running with PID $pid";
+		}
+		warn "Removing PID file for defunct server process $pid.\n";
+		unless(-w $file && unlink $file) {
+			die "Can't unlink PID file $file"
+		}
+	}
+	if($fh = IO::File->new($file,O_WRONLY|O_CREAT|O_EXCL,0644)) {
+		return($fh);
+	}
+	else {
+		die "Can't create $file: $!\n";
+	}
+}
+
+sub hostcheck {
+	my $node = shift;
+	my $nodename;
+	my $attributes;
+
+	#TBD, check DNS or xCAT for valid
+
+	return(1);
+}
+
+sub logger {
+	my $type = shift;
+	my $msg = shift;
+
+	setlogsock('unix');
+	openlog('autogpfsd','','local0');
+	syslog($type,$msg);
+	closelog();
+}
+
+END { unlink PIDFILE if $$ == $pid; }
+
