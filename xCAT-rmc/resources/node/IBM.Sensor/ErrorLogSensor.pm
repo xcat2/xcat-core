be4d2366b6425da3a4bc6513bf0e3e95337c7c69
linggao <linggao@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2009-06-12 19:20:00 +0000

    added support to install predefined sensors to nodes for RMC monitoring
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@3564 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-rmc/resources/node/IBM.Sensor/ErrorLogSensor.pm
+++ xCAT-rmc/resources/node/IBM.Sensor/ErrorLogSensor.pm
@@ -1,7 +1,15 @@
 #!/usr/bin/perl
+
+my $cmd;
+if (-r "/var/xcat/rmcmon/scripts/monerrorlog") {
+   $cmd="/var/xcat/rmcmon/scripts/monerrorlog";
+} else {
+   $cmd="/opt/xcat/sbin/rmcmon/monerrorlog";
+}
+
 $RES::Sensor{'ErrorLogSensor'} = {
 	Name => q(ErrorLogSensor),
-	Command => "/etc/xcat/rmcmon/scripts/monerrorlog",
+	Command => "$cmd",
 	UserName => q(root),
 	RefreshInterval => q(60),
 	ControlFlags => q(4),
