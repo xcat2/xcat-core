5e462c7a75c2ba682fa58ff5e0d8d0b6103533d4
ligc <ligc@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2009-06-30 11:44:14 +0000

    update nodegroup manpage for the selection string syntax change
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@3669 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -276,7 +276,7 @@ nodegroup => {
   groupname => 'Name of the group.',
   grouptype => 'The only current valid value is dynamic.  We will be looking at having the object def commands working with static group definitions in the nodelist table.',
   members => 'The value of the attribute is not used, but the attribute is necessary as a place holder for the object def commands.  (The membership for static groups is stored in the nodelist table.)',
-  wherevals => 'A list of comma-separated "attr=val" pairs that can be used to determine the members of a dynamic group.',
+  wherevals => 'A list of "attr*val" pairs that can be used to determine the members of a dynamic group, the delimiter is "::" and the operator * can be ==, =~, != or !~.',
      comments => 'Any user-written notes.',
      disable => "Set to 'yes' or '1' to comment out this row.",
  },
