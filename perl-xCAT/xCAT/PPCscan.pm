e1515f211aa03f837ba8d0c5247e85cf92754420
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2009-07-29 16:09:32 +0000

    remove Linux check around XML::Simple::PREFERRED_PARSER
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@3896 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/PPCscan.pm
+++ perl-xCAT/xCAT/PPCscan.pm
@@ -5,9 +5,7 @@ use strict;
 use Getopt::Long;
 use Socket;
 use XML::Simple;
-if ($^O =~ /^linux/i) {
-	$XML::Simple::PREFERRED_PARSER='XML::Parser';
-}
+$XML::Simple::PREFERRED_PARSER='XML::Parser';
 use xCAT::PPCcli qw(SUCCESS EXPECT_ERROR RC_ERROR NR_ERROR);
 use xCAT::PPCdb;
 use xCAT::GlobalDef;
