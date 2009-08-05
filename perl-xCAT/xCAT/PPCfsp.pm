a290222c6239108a869780998a151213e9b3dca2
zhanx <zhanx@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2009-08-05 11:47:33 +0000

    Tried to fix bug 2824608   rspconfig should be able to update /etc/hosts for network op. But finally commented the changes, because didn't get a good solution
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@3944 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/PPCfsp.pm
+++ perl-xCAT/xCAT/PPCfsp.pm
@@ -1818,6 +1818,16 @@ sub set_netcfg
     #xCAT::MsgUtils->message("I", "Updating network configuration for node " . $exp->[1] . "...");
     $ua->timeout( 10);
     $res = $ua->request( $data);
+#We should update the /etc/hosts for users, but 
+#following line can cause problem, because multiple processes will try to
+#update /etc/hosts concurrently. And for 0.0.0.0, what should we do?
+#Comment these lines
+#    if( $inc_ip and $inc_ip ne '0.0.0.0')
+#    {
+#        my %host_ip;
+#        $host_ip{$exp->[1]} = $inc_ip;
+#        xCAT::Utils::updateEtcHosts(\%host_ip);
+#    }
     ##############################################################
     # We cannot get the result of this update, since the network
     # is updated, the old URI is invalid anymore
