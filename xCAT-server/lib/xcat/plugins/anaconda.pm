6f457f0685e298d6b3e9639bb52bfa46475bd879
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2009-09-01 17:50:13 +0000

    missing $ on node made the default of  to xcatmaster not work.  The result was if you did not define a noderes.tftpserver,  you got an error runnin nodeset blade01 netboot
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@4068 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/anaconda.pm
+++ xCAT-server/lib/xcat/plugins/anaconda.pm
@@ -294,7 +294,7 @@ sub mknetboot
         }
         else
         {
-            $ient = $reshash->{node}->[0]; #$restab->getNodeAttribs($node, ['xcatmaster']);
+            $ient = $reshash->{$node}->[0]; #$restab->getNodeAttribs($node, ['xcatmaster']);
             if ($ient and $ient->{xcatmaster})
             {
                 $imgsrv = $ient->{xcatmaster};
