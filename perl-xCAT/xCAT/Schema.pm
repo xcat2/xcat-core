958c6f6262e44c10747b3def384c68503eae9f86
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2009-09-21 18:43:10 +0000

    fix more comments
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@4193 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -552,7 +552,7 @@ site => {
  descriptions => {
   key => "Name of the attribute:\tDescription\n".
    "  blademaxp:\tThe maximum number of processes for blade hardware control. Default is 64.\n\n".
-   "  consoleondemand:\tWhen set to 'yes', conserver connects and creates the console output only when the user opens the console. Default is no.\n\n".
+   "  consoleondemand:\tWhen set to 'yes', conserver connects and creates the console output only when the user opens the console. Default is no on Linux, yes on AIX.\n\n".
    "  defserialflow:\tThe default serial flow - currently only used by the mknb command.\n\n".
    "  defserialport:\tThe default serial port - currently only used by mknb.\n\n".
    "  defserialspeed:\tThe default serial speed - currently only used by mknb.\n\n".
