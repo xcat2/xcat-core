4bdd96e561b717eb9bd6a3dbff70c0e2650b2ee1
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-01-27 14:40:55 +0000

    add script for setting up xcatdb in DB2
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@5059 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

---
+++ xCAT-server/share/xcat/tools/createdb.sql
@@ -0,0 +1,25 @@
+create db xcatdb on /var/lib/db2/data;
+
+update dbm cfg using dft_mon_bufpool on dft_mon_lock on dft_mon_sort on dft_mon_stmt on dft_mon_table on dft_mon_uow on health_mon on;
+update dbm cfg using java_heap_sz 4096 num_initagents 2 max_querydegree any intra_parallel yes;
+update dbm cfg using MON_HEAP_SZ 256;
+update dbm cfg using MAXAGENTS 500;
+update dbm cfg using NUM_POOLAGENTS 250;
+update dbm cfg using FCM_NUM_BUFFERS 4096;
+
+connect to xcatdb;
+
+update db cfg for xcatdb using LOGBUFSZ 98;
+update db cfg for xcatdb using LOCKLIST 2087;
+update db cfg for xcatdb using APPGROUP_MEM_SZ 40000;
+update db cfg for xcatdb using GROUPHEAP_RATIO 80;
+update db cfg for xcatdb using APP_CTL_HEAP_SZ 512;
+update db cfg for xcatdb using MAXLOCKS 100;
+update db cfg for xcatdb using NUM_IOSERVERS 4;
+update db cfg for xcatdb using LOGFILSIZ 10024;
+update db cfg for xcatdb using LOGPRIMARY 10;
+update db cfg for xcatdb using LOGSECOND 20;
+update db cfg for xcatdb using SOFTMAX 120;
+update db cfg for xcatdb using DBHEAP 2500;
+update db cfg for xcatdb using STMTHEAP 4096;
+update db cfg for xcatdb using APPLHEAPSZ 2500;
