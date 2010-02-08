b34ea1f3aad23f3743da218a546344c070888d05
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-02-08 13:56:22 +0000

    add creation of 32K bufferspace and tablespace to createdb db2 script
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@5188 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/share/xcat/tools/createdb.sql
+++ xCAT-server/share/xcat/tools/createdb.sql
@@ -1,4 +1,6 @@
 create db xcatdb on /var/lib/db2/data;
+CREATE BUFFERPOOL BP32K SIZE 2000 PAGESIZE 32K
+Create tablespace xcattbs32k pagesize 32k managed by system using('/var/lib/db2/ts32')  BUFFERPOOL BP32K
 
 update dbm cfg using dft_mon_bufpool on dft_mon_lock on dft_mon_sort on dft_mon_stmt on dft_mon_table on dft_mon_uow on health_mon on;
 update dbm cfg using java_heap_sz 4096 num_initagents 2 max_querydegree any intra_parallel yes;
