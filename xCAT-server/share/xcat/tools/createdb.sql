38dc6fdadb61a7817f34bd071b10c444d26e741d
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-02-16 16:57:37 +0000

    remove general db2 setup
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@5228 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/share/xcat/tools/createdb.sql
+++ xCAT-server/share/xcat/tools/createdb.sql
@@ -1,14 +1,5 @@
 create db xcatdb on /var/lib/db2/data;
-
-update dbm cfg using dft_mon_bufpool on dft_mon_lock on dft_mon_sort on dft_mon_stmt on dft_mon_table on dft_mon_uow on health_mon on;
-update dbm cfg using java_heap_sz 4096 num_initagents 2 max_querydegree any intra_parallel yes;
-update dbm cfg using MON_HEAP_SZ 256;
-update dbm cfg using MAXAGENTS 500;
-update dbm cfg using NUM_POOLAGENTS 250;
-update dbm cfg using FCM_NUM_BUFFERS 4096;
-
 connect to xcatdb;
-
 CREATE BUFFERPOOL BP16K SIZE 2000 PAGESIZE 16K;
 Create tablespace xcattbs16k pagesize 16k managed by system using('/var/lib/db2/ts16')  BUFFERPOOL BP16K;
 CREATE BUFFERPOOL BP32K SIZE 2000 PAGESIZE 32K;
