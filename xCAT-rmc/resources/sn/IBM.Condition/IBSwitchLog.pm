d84fb6810618ff2c0286a802b54cfa98351f42b2
linggao <linggao@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-03-02 17:13:59 +0000

    fixed a defect for IB monitoring
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@5335 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-rmc/resources/sn/IBM.Condition/IBSwitchLog.pm
+++ xCAT-rmc/resources/sn/IBM.Condition/IBSwitchLog.pm
@@ -4,7 +4,7 @@ $RES::Condition{'IBSwitchLog'} = {
         Name => q(IBSwitchLog),
         ResourceClass => q(IBM.Sensor),
         EventExpression => q(String != ""),
-        EventDescription => q(An event will be generated when an error is logged to the Syslog in the local node for IB.),
+        EventDescription => q(An event will be generated when errors are logged to the Syslog in the local node for IB. The errors are saved in the String attribute in the event. However, if the String attribute in the event starts with XCAT_MONAIXSYSLOG_FILE:filename, then the errors can be found in the file. In this case, it is the responsibility of the response that associates with the condition to remove the temporary file.),
         SelectionString => q(Name="IBSwitchLogSensor"),
         Severity => q(0),
 };
--- xCAT-rmc/resources/sn/IBM.Sensor/IBSwitchLogSensor.pm
+++ xCAT-rmc/resources/sn/IBM.Sensor/IBSwitchLogSensor.pm
@@ -11,7 +11,8 @@ else {$cmd="$::XCATROOT/sbin/rmcmon/monaixsyslog";}
 
 $RES::Sensor{'IBSwitchLogSensor'} = {
 	Name => q(IBSwitchLogSensor),
-	Command => "$cmd -p local6.info",
+	Command => "$cmd -p local6.warn -f /var/log/xcat/syslog.fabric.warning",
+	Description => "This sensor monitors the errors logged by IB Switch management software. The String attribute will get updated with the IB related errors happend within the last 60 seconds.  If the length of the error messages is too long, the errors will be saved into a temporary file under /var/opt/xcat_aix_syslog. And the String attrubute will be updated with the file name instead. The format is XCAT_MONAIXSYSLOG_FILE:filename.",
 	UserName => q(root),
 	RefreshInterval => q(60),
         ErrorExitValue => q(1),
