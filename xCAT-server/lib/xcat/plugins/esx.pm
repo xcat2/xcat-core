95cae1dacd6e0e05de2d59e5add0614fcf735c35
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-03-23 23:15:09 +0000

    -Add more checks for a question actually existing before looking for it
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@5565 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/esx.pm
+++ xCAT-server/lib/xcat/plugins/esx.pm
@@ -442,7 +442,7 @@ sub process_tasks {
             if ($state eq 'running' and not $running_tasks{$_}->{questionasked}) { # and $curt->info->progress == 95) { #This is unfortunate, there should be a 'state' to indicate a question is blocking
                     #however there isn't, so if we see something running at 95%, we just manually see if a question blocked the rest
                     my $vm = $curcon->get_view(mo_ref=>$curt->info->entity);
-                    if ($vm->{summary} and $vm->summary->runtime->question) {
+                    if ($vm->{summary} and  $vm->summary->{runtime} and $vm->summary->runtime->{question} and $vm->summary->runtime->question) {
                         $running_tasks{$_}->{questionasked}=1;
                          $running_tasks{$_}->{callback}->($curt,$running_tasks{$_}->{data},$vm->summary->runtime->question,$vm);
                     } 
