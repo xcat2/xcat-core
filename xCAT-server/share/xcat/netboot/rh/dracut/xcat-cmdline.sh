d21b54c9fb52068cc9c167f2f68e0c6823b8c41e
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-05-11 13:44:34 +0000

    -Honor -i and remove hardcoded IFACES
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@6059 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/share/xcat/netboot/rh/dracut/xcat-cmdline.sh
+++ xCAT-server/share/xcat/netboot/rh/dracut/xcat-cmdline.sh
@@ -1,5 +1,4 @@
 root=1
 rootok=1
-IFACES=eth0 #THIS WILL SOURCE FROM /procm/cmdline if genimage -i argument omitted, TODO
 netroot=xcat
 echo '[ -e $NEWROOT/proc ]' > /initqueue-finished/xcatroot.sh
