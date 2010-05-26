65025c463289a2bafea92eb2fd106d2cdc34869a
jet777 <jet777@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-05-26 19:08:30 +0000

    feature 3006951
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@6250 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

---
+++ xCAT/templates/cmos_settings/nosol/default/hs22.bat
@@ -0,0 +1,8 @@
+loaddefault uEFI
+loaddefault BootOrder
+set BootOrder.BootOrder "Legacy Only=PXE Network=Hard Disk 0"
+set uEFI.RemoteConsoleRedirection Enable
+set uEFI.Com2TextEmul VT100
+set uEFI.Com2ActiveAfterBoot Enable
+set uEFI.Com2FlowControl Hardware
+set uEFI.LegacyOptionRomPort "COM Port 1"
