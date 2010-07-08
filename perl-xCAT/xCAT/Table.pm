e032aa937534bf28d3502904186a7c820d6e0fc0
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-07-08 20:49:12 +0000

    -Nonstop xCAT, xcatd starting replaces old xCAT instance with nearly no interruption
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@6680 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Table.pm
+++ perl-xCAT/xCAT/Table.pm
@@ -67,7 +67,7 @@ require xCAT::NotifHandler;
 
 my $dbworkerpid; #The process id of the database worker
 my $dbworkersocket;
-my $dbsockpath = "/tmp/xcat/dbworker.sock";
+my $dbsockpath = "/tmp/xcat/dbworker.sock.".$$;
 my $exitdbthread;
 my $dbobjsforhandle;
 my $intendedpid;
