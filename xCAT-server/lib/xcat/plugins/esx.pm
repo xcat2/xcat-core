7a7c11650ceeeb9e969aef6b5850eb106f9d5876
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-07-09 17:02:25 +0000

    -Correct mistake in previous optimization
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@6694 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/esx.pm
+++ xCAT-server/lib/xcat/plugins/esx.pm
@@ -865,15 +865,15 @@ sub get_hostview {
         push @matchvalues,$_;
     }
     my $view;
-    $subargs{filter}={'name' =~ qr/$host(?:\.|\z)/};
+    $subargs{filter}={'name' => qr/$host(?:\.|\z)/};
     $view = $args{conn}->find_entity_view(%subargs);
     if ($view) { return $view; }
     foreach (@matchvalues) {
-        $subargs{filter}={'name' =~ qr/$_(?:\.|\z)/};
+        $subargs{filter}={'name' => qr/$_(?:\.|\z)/};
         $view = $args{conn}->find_entity_view(%subargs);
         if ($view) { return $view; }
     }
-    $subargs{filter}={'name' =~ qr/localhost(?:\.|\z)/};
+    $subargs{filter}={'name' => qr/localhost(?:\.|\z)/};
     $view = $args{conn}->find_entity_view(%subargs);
     if ($view) { return $view; }
     return undef; #rest of function should be obsoleted, going to run with that assumption for 2.5 at least
