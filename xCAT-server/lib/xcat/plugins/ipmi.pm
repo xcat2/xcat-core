2e261d1cc85b48a70c0727ef42b2e0535c25d467
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-07-22 15:37:12 +0000

    -Further clean up the message
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@6830 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/ipmi.pm
+++ xCAT-server/lib/xcat/plugins/ipmi.pm
@@ -2112,7 +2112,7 @@ sub add_fruhash {
             my $err;
             ($err,$fruhash) = parsefru($sessdata->{currfrudata});
             if ($err) {
-                sendmsg([1,"Error reading fru area ".$sessdata->{currfruid}.": $err"]);
+                sendmsg([1,":Error reading fru area ".$sessdata->{currfruid}.": $err"]);
                 return;
             }
     }
