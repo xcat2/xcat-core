583d784e1f81b9b96d02cd561ae8e1743f7ea26b
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-08-11 12:13:37 +0000

    add LL DBM setup
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@7040 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

---
+++ xCAT-server/share/xcat/tools/updateDBM.sql
@@ -0,0 +1,8 @@
+update dbm cfg  using MON_HEAP_SZ AUTOMATIC;
+update dbm cfg using INSTANCE_MEMORY AUTOMATIC;
+update dbm cfg  using NUM_POOLAGENTS AUTOMATIC;
+update dbm cfg using MAX_COORDAGENTS AUTOMATIC;
+update dbm cfg using MAX_CONNECTIONS AUTOMATIC;
+update dbm cfg using FENCED_POOL AUTOMATIC;
+update dbm cfg using FCM_NUM_BUFFERS AUTOMATIC;
+update dbm cfg using FCM_NUM_CHANNELS AUTOMATIC;
