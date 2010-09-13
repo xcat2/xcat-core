b89f5e1a5be96935b3ffbe9b77dcd44136c621f8
xq2005 <xq2005@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-09-13 08:36:05 +0000

    add Authenticated check
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@7426 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-UI/lib/systemcmd.php
+++ xCAT-UI/lib/systemcmd.php
@@ -12,6 +12,10 @@ require_once "$TOPDIR/lib/jsonwrapper.php";
  * @param 	$cmd	The system command
  * @return 	The system response.  Replies are in the form of JSON
  */
+if (!isAuthenticated()){
+	echo ("<b>Please log in from the main page first!</b>");
+	exit;
+}
 
 if (isset($_GET["cmd"])) {
     // HTTP GET requests
