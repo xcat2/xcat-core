2480d30d3166839502a0e7cb2be34a71335969c1
linggao <linggao@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-09-17 17:51:39 +0000

    added more in imgport.pm
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@7507 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/imgport.pm
+++ xCAT-server/lib/xcat/plugins/imgport.pm
@@ -249,7 +249,7 @@ sub get_image_info {
 	}
 	
         #from linuximage table
-	(my $attrs1) = $linuximagetab->getAttribs({imagename => $imagename}, 'template', 'pkglist', 'pkgdir', 'otherpkglist', 'otherpkgdir', 'exlist', 'postinstall', 'rootimgdir', 'nodebootif', 'otherifce', 'netdrivers', 'kernelver');
+	(my $attrs1) = $linuximagetab->getAttribs({imagename => $imagename}, 'template', 'pkglist', 'pkgdir', 'otherpkglist', 'otherpkgdir', 'exlist', 'postinstall', 'rootimgdir', 'nodebootif', 'otherifce', 'netdrivers', 'kernelver', 'permission');
 	if (!$attrs1) {
 		$callback->({error=>["Cannot find image \'$imagename\' from the linuximage table."],errorcode=>[1]});
 		return 0;
@@ -1055,6 +1055,9 @@ sub set_config {
 	};
         if ($data->{kernelver}) {
 	    $keyhash{kernelver} = $data->{kernelver};
+	};
+        if ($data->{permission}) {
+	    $keyhash{permission} = $data->{permission};
 	};
         if ($data->{nodebootif}) {
 	    $keyhash{nodebootif} = $data->{nodebootif};
