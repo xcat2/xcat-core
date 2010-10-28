b5eff3d8a5d6c5ebbea10ee72f45d089e928cc18
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2010-10-28 17:27:48 +0000

    change sshbetweennodes attr
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@7957 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -781,9 +781,9 @@ site => {
    " ppctimeout:  The timeout, in milliseconds, to use when communicating with PPC hw.\n\n".
    " pruneservices:  Whether to enable service pruning when noderm is run (i.e.\n".
    "                 removing DHCP entries when noderm is executed)\n\n".
-   " sshbetweennodes:  Whether to enable passwordless root ssh between nodes\n".
-   "                   during node install, or xdsh -K. Default is yes.\n".
-   "                   Set to no, if you do not want enabled.\n".
+   " sshbetweennodes:  List of groups to enable passwordless root ssh \n".
+   "                   during install, or xdsh -K. Default is ALLGROUPS.\n".
+   "                   Set to NOGROUPS,if you do not wish to enabled any groups.\n".
    "                   Service Nodes are not affected by this attribute\n".
    "                   they are always setup with\n".
    "                   passwordless root access to nodes and other SN.\n\n".
