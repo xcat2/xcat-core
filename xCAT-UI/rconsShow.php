061812e66bb40a2fab142a61a8da15ac7c09bb7c
xq2005 <xq2005@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-03-30 09:17:29 +0000

    change for jquery version update
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@9182 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-UI/rconsShow.php
+++ xCAT-UI/rconsShow.php
@@ -5,7 +5,7 @@ echo <<<EEE
 	    <title>{$_GET['rconsnd']}</title>
 	    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
 	    <link rel="stylesheet" type="text/css" href="css/ajaxterm.css"/>
-	    <script type="text/javascript" src="js/jquery/jquery-1.4.2.min.js"></script>
+	    <script type="text/javascript" src="js/jquery/jquery-1.4.4.min.js"></script>
 	    <script type="text/javascript" src="js/rcons/rcons.js"></script>
 	    <script type="text/javascript">
 	    window.onload=function() {
