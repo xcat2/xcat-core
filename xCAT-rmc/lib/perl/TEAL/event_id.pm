c3f6cce8901de5a3425707e4abd3e72d9cc5254e
ellen56 <ellen56@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-04-27 06:40:59 +0000

    add code for teal event table event_id mapping, feature #3166402
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@9409 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

---
+++ xCAT-rmc/lib/perl/TEAL/event_id.pm
@@ -0,0 +1,47 @@
+# IBM_PROLOG_BEGIN_TAG 
+# This is an automatically generated prolog. 
+#  
+#  
+#  
+# Licensed Materials - Property of IBM 
+#  
+# (C) COPYRIGHT International Business Machines Corp. 2010,2011 
+# All Rights Reserved 
+#  
+# US Government Users Restricted Rights - Use, duplication or 
+# disclosure restricted by GSA ADP Schedule Contract with IBM Corp. 
+#  
+# IBM_PROLOG_END_TAG 
+package TEAL::event_id;
+
+# This file contains the schema necessary to support the TEAL eventid
+
+%tabeventid = (
+  AIXNodeCoreDump => "XR000001",
+  AllServiceableEvents => "XR000002",
+  AnyNodeAnyLoggedError => "XR000003",
+  AnyNodeFileSystemInodesUsed => "XR000004",
+  AnyNodeFileSystemSpaceUsed => "XR000005",
+  AnyNodeNetworkInterfaceStatus => "XR000006",
+  AnyNodePagingPercentSpaceFree => "XR000007",
+  AnyNodeProcessorsIdleTime => "XR000008",
+  AnyNodeRealMemFree => "XR000009",
+  AnyNodeTmpSpaceUsed => "XR000010",
+  AnyNodeVarSpaceUsed => "XR000011",
+  CheckCONSonSN => "XR000012",
+  CheckDHCPonSN => "XR000013",
+  CheckFTPonSN => "XR000014",
+  CheckNAMEDonSN => "XR000015",
+  CheckNFSonSN => "XR000016",
+  CheckNTPonSN => "XR000017",
+  CheckTFTPonSN => "XR000018",
+  CheckxCATonSN => "XR000019",
+  Drawer_not_configured => "XR000020",
+  HFI_down => "XR000021",
+  HFI_not_configured => "XR000022",
+  IBSwitchLog => "XR000023",
+  NodeReachability => "XR000024",
+  VarSpaceUsed => "XR000025"
+);
+
+1;    
