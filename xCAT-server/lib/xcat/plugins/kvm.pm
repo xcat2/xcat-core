0ecfd253ebbf12e73bd1f88a64f8e96b27d35476
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-05-02 19:10:54 +0000

    Don't try changing listen parameters on getrvidparms
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@9464 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/kvm.pm
+++ xCAT-server/lib/xcat/plugins/kvm.pm
@@ -675,7 +675,7 @@ sub getcons {
     } elsif ($type eq "vid") {
       my $tpasswd=genpassword(10);
       my $validto=POSIX::strftime("%Y-%m-%dT%H:%M:%S",gmtime(time()+300));
-	$dom->update_device("<graphics type='".$consdata->{vidproto}."' listen='0.0.0.0' passwd='$tpasswd' passwdValidTo='$validto'/>");
+	$dom->update_device("<graphics type='".$consdata->{vidproto}."' passwd='$tpasswd' passwdValidTo='$validto'/>");
 	$consdata->{password}=$tpasswd;
 	$consdata->{server}=$hyper;
 	return $consdata;
