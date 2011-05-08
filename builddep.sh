f6907c7f3df8652a2d55a91f6d12dfc1afee6dd6
xq2005 <xq2005@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-05-08 05:32:32 +0000

    does not install xCAT-UI-deps in instoss
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@9511 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- builddep.sh
+++ builddep.sh
@@ -112,7 +112,7 @@ cd $OSVER
 #  pyodbc is dependent on unixODBC
 rpm -Uvh unixODBC*
 
-for i in `ls *.rpm|grep -v -E '^tcl-|^tk-|^expect-|^unixODBC-'|^xCAT-UI-deps`; do
+for i in `ls *.rpm|grep -v -E '^tcl-|^tk-|^expect-|^unixODBC-|^xCAT-UI-deps'`; do
 	if [ "$i" == "perl-Net-DNS-0.66-1.aix5.3.ppc.rpm" ]; then
 		opts="--nodeps"
 	else
