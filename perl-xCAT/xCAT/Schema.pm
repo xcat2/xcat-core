3e5ebbd344407b3f4fa110712cc15f0083367174
phamt <phamt@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-05-17 19:46:35 +0000

    Add support for System z hierarchy.
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@9622 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -1056,13 +1056,15 @@ routes => {
 },
 
 zvm => {
-	cols => [qw(node hcp userid comments disable)],
+	cols => [qw(node hcp userid nodetype parent comments disable)],
 	keys => [qw(node)],
 	table_desc => 'List of z/VM virtual servers.',
 	descriptions => {
 		node => 'The node name.',
 		hcp => 'The hardware control point for this node.',
 		userid => 'The z/VM userID of this node.',
+		nodetype => 'The node type. Valid values: cec (Central Electronic Complex), lpar (logical partition), zvm (z/VM host operating system), and vm (virtual machine).',
+		parent => 'The parent node. For LPAR, this specifies the CEC. For z/VM, this specifies the LPAR. For VM, this specifies the z/VM host operating system.',
 		comments => 'Any user provided notes.',
 		disable => "Set to 'yes' or '1' to comment out this row.",
 	},
