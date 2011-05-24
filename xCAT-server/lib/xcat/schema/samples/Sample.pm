6095d5af97bc81bb55346e6e672305dcc47cc69f
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-05-24 16:59:06 +0000

    update Sample with compress=> support for DB2
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@9672 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/schema/samples/Sample.pm
+++ xCAT-server/lib/xcat/schema/samples/Sample.pm
@@ -57,6 +57,7 @@ package xCAT_schema::Sample;
 	    jobid => 'INTEGER',
 	    cpu_usage => 'FLOAT',
 	},
+        compress =>'YES',  # note compress only for DB2
         tablespace =>'XCATTBS32K',  # note tablespace only for DB2
         table_desc => 'Stores the node status.',
         descriptions => {
