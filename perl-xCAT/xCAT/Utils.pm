6db4432ce3d416caaba2d92056fce8c4507535c6
mellor <mellor@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-07-28 00:04:31 +0000

    defect 3377019 - setAppStatus - do not set appstatus if node is blank
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@10179 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Utils.pm
+++ perl-xCAT/xCAT/Utils.pm
@@ -6233,6 +6233,7 @@ sub setAppStatus
 
     my %new_nodeappstat;
     foreach my $node (keys %$nodeappstat) {
+        if ( $node =~ /^\s*$/ ) { next; }  # Skip blank node names 
         my $new_appstat = "";
         my $changed = 0;
 
