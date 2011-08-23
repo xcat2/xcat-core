5ceedcdb2666dc32dc7d4fe484091bfe8e685b58
phamt <phamt@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-08-23 14:16:56 +0000

    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@10345 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-UI/js/custom/ipmi.js
+++ xCAT-UI/js/custom/ipmi.js
@@ -477,7 +477,7 @@ function createProvisionExisting(inst) {
 				data : {
 					cmd : 'nodeadd',
 					tgt : '',
-					args : tgts + ';noderes.netboot=zvm;nodetype.os=' + os.val() + ';nodetype.arch=' + arch.val() + ';nodetype.profile=' + profile.val(),
+					args : tgts + ';noderes.netboot=xnba;nodetype.os=' + os.val() + ';nodetype.arch=' + arch.val() + ';nodetype.profile=' + profile.val(),
 					msg : 'cmd=nodeadd;out=' + inst
 				},
 
