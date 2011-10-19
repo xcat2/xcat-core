e9cc31faaeee26b1316da759dfe51d02f149ecf2
jjhua <jjhua@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2011-10-19 05:35:41 +0000

    remove the  from the get_ServiceNode() which was added by me
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/branches/2.6@10836 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Utils.pm
+++ perl-xCAT/xCAT/Utils.pm
@@ -3616,7 +3616,7 @@ sub get_site_Master
 sub get_ServiceNode
 {
     require xCAT::Table;
-    my ($class, $node, $service, $request, $onlymaster) = @_;
+    my ($class, $node, $service, $request) = @_;
 
     my @node_list = @$node;
     my $cmd;
@@ -3635,15 +3635,7 @@ sub get_ServiceNode
 
     if ($request eq "MN")
     {
-        #When run mkhwconn/lshwconn/rmhwconn with -T fnm for CNM, it will send the
-        #command from the MN to CEC/Frame direclty, not through the service node if specified.
-        #The $onlymaster is set in preprocess_request() in PPC.pm
-        #It will not affect the others
-        if( defined($onlymaster) && $onlymaster == 1) {
-            $snattribute = "xcatmaster";
-	    } else {
-	        $snattribute = "servicenode";
-	    }
+        $snattribute = "servicenode";
     
     }
     else    # Node
