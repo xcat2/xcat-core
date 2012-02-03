cd5ee8a18c8db7d481cafeb2b1a0b4bfe4b0473b
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2012-02-03 21:24:31 +0000

    Fix brace mismatch in mknb
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@11474 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/mknb.pm
+++ xCAT-server/lib/xcat/plugins/mknb.pm
@@ -126,6 +126,7 @@ sub process_request {
        $callback->({data=>["Creating genesis.fs.$arch.gz in $tftpdir/xcat"]});
        system("cd $tempdir; find . | cpio -o -H newc | gzip -9 > $tftpdir/xcat/genesis.fs.$arch.gz");
 	}
+     }
    } else {
    	$callback->({data=>["Creating nbfs.$arch.gz in $tftpdir/xcat"]});
        system("cd $tempdir; find . | cpio -o -H newc | gzip -9 > $tftpdir/xcat/nbfs.$arch.gz");
