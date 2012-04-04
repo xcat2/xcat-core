bf841a2946daeb208833d5668e6c7efb4835cc81
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2012-04-04 14:59:07 +0000

    fix tabch -h
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@12117 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/tabutils.pm
+++ xCAT-server/lib/xcat/plugins/tabutils.pm
@@ -2024,11 +2024,16 @@ sub tabch {
         my %rsp;
         push @{$rsp{data}}, "Usage: tabch";
         push @{$rsp{data}}, "       To add or update rows for tables:";
-        push @{$rsp{data}}, "       chtab [keycolname=keyvalue[,keycolname=keyvalue...]] [tablename.colname=newvalue] [tablename.colname=newvalue]...";
+        push @{$rsp{data}}, "       tabch [keycolname=keyvalue[,keycolname=keyvalue...]] [tablename.colname=newvalue] [tablename.colname=newvalue]...";
         push @{$rsp{data}}, "       To delete rows from tables:";
-        push @{$rsp{data}}, "       chtab -d|--delete keycolname=keyvalue[,keycolname=keyvalue...] tablename [tablename]...";
-        push @{$rsp{data}}, "       keycolname=keyvalue   a column name-and-value pair that identifies the rows in a table to be changed.
-   tablename.colname=newvalue    the new value for the specified row and column of the table.";
+        push @{$rsp{data}}, "       tabch -d|--delete keycolname=keyvalue[,keycolname=keyvalue...] tablename [tablename]...";
+        push @{$rsp{data}}, "         keycolname=keyvalue   a column name-and-value pair ";
+        push @{$rsp{data}}, "         that identifies the rows in a table to be changed.";
+        push @{$rsp{data}}, "         a column name-and-value pair that identifies ";
+        push @{$rsp{data}}, "         the rows in a table to be changed.";
+        push @{$rsp{data}}, "         that identifies the rows in a table to be changed.";
+        push @{$rsp{data}}, "         tablename.colname=newvalue ";
+        push @{$rsp{data}}, "         the new value for the specified row and column of the table.";
         push @{$rsp{data}}, "       tabprune [-h|--help]";
         push @{$rsp{data}}, "       tabprune [-v|--version]";
         if ($exitcode) { $rsp{errorcode} = $exitcode; }
