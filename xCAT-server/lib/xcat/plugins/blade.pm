446d5225d6f28ce163e526731a9a03f9b6a6bdc2
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2012-05-31 14:12:51 +0000

    Fix blade.pm problem
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/branches/2.7@12972 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/blade.pm
+++ xCAT-server/lib/xcat/plugins/blade.pm
@@ -3847,7 +3847,10 @@ sub process_request {
       %macmap = ();
       $macmaptimestamp=time();
       $indiscover=1;
-      foreach (@{preprocess_request(\%invreq,\&fillresps)}) {
+      my $reqs = preprocess_request(\%invreq,\&fillresps);
+      my @reql;
+      if ($reqs) { @reql = @{$reqs}; }
+      foreach (@reql) {
          %invreq = %$_;
          process_request(\%invreq,\&fillresps);
       }
