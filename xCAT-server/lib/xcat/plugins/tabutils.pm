56ab665d2a5fef1b741ffba91c5a3ecac0a589c9
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2012-09-13 18:24:34 +0000

    change name
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@13787 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/tabutils.pm
+++ xCAT-server/lib/xcat/plugins/tabutils.pm
@@ -59,7 +59,7 @@ sub handled_commands
             getAllEntries    => "tabutils",
             getNodesAttribs  => "tabutils",
             getTablesAllNodeAttribs  => "tabutils",
-            getTablesNodeAttribs  => "tabutils",
+            getTablesNodesAttribs  => "tabutils",
             setNodesAttribs  => "tabutils",
             delEntries       => "tabutils",
             getAttribs       => "tabutils",
@@ -163,9 +163,9 @@ sub process_request
     {
         return getTablesAllNodeAttribs($request,$callback);
     }
-    elsif ($command eq "getTablesNodeAttribs")
+    elsif ($command eq "getTablesNodesAttribs")
     {
-        return getTablesNodeAttribs($request,$callback);
+        return getTablesNodesAttribs($request,$callback);
     }
     elsif ($command eq "setNodesAttribs")
     {
@@ -2482,14 +2482,14 @@ sub getTablesAllNodeAttribs
         return;
 }
 
-# getTablesNodeAttribs 
+# getTablesNodesAttribs 
 # Read the nodes in the noderange from the input tables
 # and get the input attributes
 # or get ALL attributes, if the word ALL is used. 
 # If the <attr>ALL</attr> is input then read all the attributes
 #<xcatrequest>
 #<clienttype>PCM</clienttype>
-#<command>getTablesAllNodeAttribs</command>
+#<command>getTablesNodesAttribs</command>
 #<noderange>blade01-blade10</noderange>
 #<table>
 #<tablename>nodelist</tablename>
@@ -2521,7 +2521,7 @@ sub getTablesAllNodeAttribs
 #   .
 #</xcatresponse>
 #
-sub getTablesNodeAttribs 
+sub getTablesNodesAttribs 
 {
     my $request      = shift;
     my $cb = shift;
