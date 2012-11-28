a7d72e8c56ac80c671bf2c9d3646c75f677d608c
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2012-11-28 18:46:31 +0000

    remove MN info on nodetype.nodetype
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@14464 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -620,7 +620,7 @@ nodetype => {
   profile => 'The string to use to locate a kickstart or autoyast template to use for OS deployment of this node.  If the provmethod attribute is set to an osimage name, that takes precendence.  Otherwise, the os, profile, and arch are used to search for the files in /install/custom first, and then in /opt/xcat/share/xcat.',
   provmethod => 'The provisioning method for node deployment. The valid values are install, netboot, statelite or an os image name from the osimage table. If an image name is specified, the osimage definition stored in the osimage table and the linuximage table (for Linux) or nimimage table (for AIX) are used to locate the files for templates, pkglists, syncfiles, etc. On Linux, if install, netboot or statelite is specified, the os, profile, and arch are used to search for the files in /install/custom first, and then in /opt/xcat/share/xcat.',
   supportedarchs => 'Comma delimited list of architectures this node can execute.',
-  nodetype => 'A comma-delimited list of characteristics of this node. The mn type, indicates it is a Management Node and should be the only attribute assigned to the MN. Valid values: ppc, blade, vm (virtual machine), osi (OS image), mm, mn, rsa, switch.',
+  nodetype => 'A comma-delimited list of characteristics of this node.  Valid values: ppc, blade, vm (virtual machine), osi (OS image), mm, mn, rsa, switch.',
      comments => 'Any user-written notes.',
      disable => "Set to 'yes' or '1' to comment out this row.",
  },
