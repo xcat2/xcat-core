7a61af5d1efe0249b976aefd9691ef4453e23078
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2013-01-02 17:20:51 +0000

    add support for updatenode -l disabled
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@14726 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Usage.pm
+++ perl-xCAT/xCAT/Usage.pm
@@ -327,7 +327,7 @@ my %usage = (
     or
     updatenode <noderange> [-V|--verbose] [-k|--security] [-s|--sn]
     or
-    updatenode <noderange> [-V|--verbose] [-F|--sync | -f|--snsync] [-S|--sw] 
+    updatenode <noderange> [-V|--verbose] [-F|--sync | -f|--snsync] [-l|--user[username]] [-S|--sw] 
         [-P|--scripts [script1,script2,...]] [-s|--sn] 
         [-A|--updateallsw] [-c|--cmdlineonly] [-d alt_source_dir]
         [attr=val [attr=val...]]
@@ -344,6 +344,8 @@ Options:
 
     [-f|--snsync] Performs File Syncing to the service nodes that service 
         the nodes in the noderange.
+    [-l|--user] User name to run the updatenode command.  It overrides the
+        current user which is the default.
 
     [-S|--sw] Perform Software Maintenance.
 
