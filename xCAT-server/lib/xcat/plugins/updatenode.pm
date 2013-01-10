18e5d75599cdea77044e6b03028959f46c72110a
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2013-01-10 16:05:38 +0000

    remove unused updatenodeupdatestat
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@14848 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/updatenode.pm
+++ xCAT-server/lib/xcat/plugins/updatenode.pm
@@ -61,7 +61,6 @@ sub handled_commands
             updatenodestat       => "updatenode",
             updatemynodestat     => "updatenode",
             updatenodeappstat    => "updatenode",
-            updatenodeupdatestat => "updatenode"
             };
 }
 
@@ -99,10 +98,6 @@ sub preprocess_request
         return [$request];
     }
     elsif ($command eq "updatenodeappstat")
-    {
-        return [$request];
-    }
-    elsif ($command eq "updatenodeupdatestat")
     {
         return [$request];
     }
@@ -158,10 +153,6 @@ sub process_request
     elsif ($command eq "updatenodeappstat")
     {
         return updatenodeappstat($request, $callback);
-    }
-    elsif ($command eq "updatenodeupdatestat")
-    {
-        return updatenodeupdatestat($request, $callback);
     }
     else
     {
@@ -1775,64 +1766,6 @@ sub updatenodestat
     return 0;
 }
 
-#-------------------------------------------------------------------------------
-
-=head3   updatenodeupdatestat
-
-update the nodelist.updatestatus and nodelist.updatestatustime during updatenode
-
-    Arguments:
-    Returns:
-        0 - for success.
-        1 - for error.
-
-=cut
-
-#-----------------------------------------------------------------------------
-sub updatenodeupdatestat
-{
-    my $request  = shift;
-    my $callback = shift;
-    my @nodes    = ();
-    my @args     = ();
-    if (ref($request->{node}))
-    {
-        @nodes = @{$request->{node}};
-    }
-    else
-    {
-        if ($request->{node}) { @nodes = ($request->{node}); }
-        else
-        {    #client asking to update its own status...
-            unless (ref $request->{username})
-            {
-                return;
-            }    #TODO: log an attempt without credentials?
-            @nodes = @{$request->{username}};
-        }
-    }
-    if (ref($request->{arg}))
-    {
-        @args = @{$request->{arg}};
-    }
-    else
-    {
-        @args = ($request->{arg});
-    }
-
-    if ((@nodes > 0) && (@args > 0))
-    {
-        my $stat = $args[0];
-        unless ($::VALID_STATUS_VALUES{$stat})
-        {
-            return;
-        }    #don't accept just any string, see GlobalDef for updates
-
-        xCAT::TableUtils->setUpdateStatus(\@nodes, $stat);
-    }
-
-    return 0;
-}
 
 #-------------------------------------------------------------------------------
 
