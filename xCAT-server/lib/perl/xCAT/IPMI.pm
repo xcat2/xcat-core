6b85f593136e94013db319782b3d2972b2516df0
jbjohnso <jbjohnso@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2013-01-29 20:10:14 +0000

    Have IPMI tolerate incorrect rqaddr field to communicate with buggy BMCs that don't comply to the spec
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/branches/2.7@15012 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/perl/xCAT/IPMI.pm
+++ xCAT-server/lib/perl/xCAT/IPMI.pm
@@ -822,7 +822,7 @@ sub parse_ipmi_payload {
     my @payload = @_;
     #for now, just trash the headers, this has been validated to death anyway
     #except seqlun, that one is important
-    unless ($payload[4] == $self->{seqlun} and $payload[0] == $self->{rqaddr} and $payload[1]>>2 == $self->{expectednetfn} and $payload[5] == $self->{expectedcmd} ) {
+    unless ($payload[4] == $self->{seqlun} and $payload[1]>>2 == $self->{expectednetfn} and $payload[5] == $self->{expectedcmd} ) {
 	#both sequence number and arqaddr must match, because we are using rqaddr to extend the sequence number
         #print "Successfully didn't get confused by stale response ".$payload[4]." and ".($self->{seqlun}-4)."\n";
         #hexdump(@payload);
