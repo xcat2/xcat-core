e469851b25cee1c5346a48f5d844c1cf76f8610a
lissav <lissav@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2013-03-01 13:23:34 +0000

    Defect 3397
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/branches/2.8@15329 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

---
+++ xCAT-server/share/xcat/scripts/updateDBM.sql
@@ -0,0 +1,10 @@
+update dbm cfg  using MON_HEAP_SZ AUTOMATIC;
+update dbm cfg using INSTANCE_MEMORY AUTOMATIC;
+update dbm cfg  using NUM_POOLAGENTS AUTOMATIC;
+update dbm cfg using MAX_COORDAGENTS AUTOMATIC;
+update dbm cfg using MAX_CONNECTIONS AUTOMATIC;
+update dbm cfg using FENCED_POOL AUTOMATIC;
+update dbm cfg using FCM_NUM_BUFFERS AUTOMATIC;
+update dbm cfg using FCM_NUM_CHANNELS AUTOMATIC;
+update dbm cfg using DIAGSIZE 1024;
+update dbm cfg using AUTHENTICATION CLIENT IMMEDIATE;
