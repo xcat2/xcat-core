50445cdc26be459ec5655e1a97b75bc94858989b
phamt <phamt@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2013-03-12 23:10:58 +0000

    Fixed cloning issue where code was looking for a case-sensitive NIC address and not modifying the network configuration correctly. Added logging of smcli calls into syslog to improve debugging. Add loop to find the correct partition with file -s /dev/dasd*. This fixes a cloning issue where flashcopy is not instantaneous and flashed copy volumes do not appear immediately when linked.
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@15470 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/zvmCPUtils.pm
+++ perl-xCAT/xCAT/zvmCPUtils.pm
@@ -468,6 +468,7 @@ sub grantVSwitch {
 
     # Use SMAPI EXEC
     my $out = `ssh $user\@$hcp "$sudo $dir/smcli Virtual_Network_Vswitch_Set -T SYSTEM -n $vswitchId -I $userId"`;
+    xCAT::zvmUtils->printSyslog("grantVSwitch- ssh $user\@$hcp $sudo $dir/smcli Virtual_Network_Vswitch_Set -T SYSTEM -n $vswitchId -I $userId");
     $out = xCAT::zvmUtils->trimStr($out);
 
     # If return string contains 'Done' - Operation was successful
@@ -564,6 +565,7 @@ sub smapiFlashCopy {
     # Use SMAPI EXEC to flash copy
     my $cmd = '\"' . "CMD=FLASHCOPY $srcId $srcAddr 0 END $tgtId $tgtAddr 0 END" . '\"';
     my $out = `ssh $user\@$hcp "$sudo $dir/smcli xCAT_Commands_IUO -T $hcpUserId -c $cmd"`;
+    xCAT::zvmUtils->printSyslog("smapiFlashCopy- ssh $user\@$hcp $sudo $dir/smcli xCAT_Commands_IUO -T $hcpUserId -c $cmd");
         
     $out = xCAT::zvmUtils->trimStr($out);
 
@@ -661,9 +663,11 @@ sub purgeReader {
         # Use SMAPI EXEC to purge reader
         my $cmd = '\"' . "CMD=PURGE $userId RDR ALL" . '\"';
         $out = `ssh $user\@$hcp "$sudo $dir/smcli xCAT_Commands_IUO -T $userId -c $cmd"`;
+        xCAT::zvmUtils->printSyslog("purgeReader- ssh $user\@$hcp $sudo $dir/smcli xCAT_Commands_IUO -T $userId -c $cmd");
     } else {
         # Purge reader using CP
         $out = `ssh -o ConnectTimeout=5 $user\@$hcp "$sudo /sbin/vmcp purge $userId rdr all"`;
+        xCAT::zvmUtils->printSyslog("purgeReader- ssh -o ConnectTimeout=5 $user\@$hcp $sudo /sbin/vmcp purge $userId rdr all");
     }
 
     $out = xCAT::zvmUtils->trimStr($out);
@@ -700,9 +704,11 @@ sub sendCPCmd {
         # Use SMAPI EXEC to send command
         $cmd = '\"' . "CMD=SEND CP $userId " . uc($cmd) . '\"';
         $out = `ssh $user\@$hcp "$sudo $dir/smcli xCAT_Commands_IUO -T $userId -c $cmd"`;
+        xCAT::zvmUtils->printSyslog("sendCPCmd- ssh $user\@$hcp $sudo $dir/smcli xCAT_Commands_IUO -T $userId -c $cmd");
     } else {
         # Send CP command to given user
         $out = `ssh $user\@$hcp "$sudo /sbin/vmcp send cp $userId $cmd"`;
+        xCAT::zvmUtils->printSyslog("sendCPCmd- ssh $user\@$hcp $sudo /sbin/vmcp send cp $userId $cmd");
     }
 
     $out = xCAT::zvmUtils->trimStr($out);
