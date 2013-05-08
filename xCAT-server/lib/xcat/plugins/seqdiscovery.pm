f6e8475fc0fd15d4f236a8d1649a072ce5631849
xq2005 <xq2005@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2013-05-08 09:50:25 +0000

    arp command location is different between redhat and debian/ubuntu
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/branches/2.8@16217 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- xCAT-server/lib/xcat/plugins/seqdiscovery.pm
+++ xCAT-server/lib/xcat/plugins/seqdiscovery.pm
@@ -71,7 +71,13 @@ sub findme {
     if (defined $request->{nodetype} and $request->{nodetype}->[0] eq 'virtual') {
         return;
     }
-    my $arptable = `/sbin/arp -n`;
+    my $arptable;
+    if ( -e "/etc/debian_version") {
+        $arptable = `/usr/sbin/arp -n`;
+    }
+    else{ 
+        $arptable = `/sbin/arp -n`;
+    }
     my @arpents = split /\n/,$arptable;
     foreach  (@arpents) {
         if (m/^($ip)\s+\S+\s+(\S+)\s/) {
