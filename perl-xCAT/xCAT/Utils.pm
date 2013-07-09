f40260f9cb3dee4eda03fa347910702eb250400a
xq2005 <xq2005@8638fb3e-16cb-4fca-ae20-7b5d299a9bcd>
2013-07-09 03:19:47 +0000

    ubuntu and debian does release SHA.pm instand of SHA1.pm
    git-svn-id: https://svn.code.sf.net/p/xcat/code/xcat-core/trunk@16908 8638fb3e-16cb-4fca-ae20-7b5d299a9bcd

--- perl-xCAT/xCAT/Utils.pm
+++ perl-xCAT/xCAT/Utils.pm
@@ -25,6 +25,9 @@ my $sha1support = eval {
 	require Digest::SHA1;
 	1;
 };
+if ( -f "/etc/debian_version" ){
+    $sha1support = eval {require Digest::SHA; 1;}
+}
 use IPC::Open3;
 use IO::Select;
 use xCAT::GlobalDef;
@@ -208,7 +211,13 @@ sub genUUID
         return $uuid;
     } elsif ($args{url} and $sha1support) { #generate a UUIDv5 from URL
         #6ba7b810-9dad-11d1-80b4-00c04fd430c8 is the uuid for URL namespace
-        my $sum = Digest::SHA1::sha1('6ba7b810-9dad-11d1-80b4-00c04fd430c8'.$args{url});
+        my $sum = '';
+        if ( -f "/etc/debian_version" ){
+            $sum = Digest::SHA::sha1('6ba7b810-9dad-11d1-80b4-00c04fd430c8'.$args{url});
+        }
+        else{
+            $sum = Digest::SHA1::sha1('6ba7b810-9dad-11d1-80b4-00c04fd430c8'.$args{url});
+        }
         my @data = unpack("C*",$sum);
         splice @data,16;
         $data[6] = $data[6] & 0xf;
