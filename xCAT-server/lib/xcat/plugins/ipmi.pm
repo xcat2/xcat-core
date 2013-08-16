ef4a9314408af9069d941621cc860e1e911bf1ae
immarvin <yangsbj@cn.ibm.comq>
2013-08-16 01:42:10 -0700

    update node provision status based on the feedback of nodes instead of rpower/rnetboot

--- xCAT-server/lib/xcat/plugins/ipmi.pm
+++ xCAT-server/lib/xcat/plugins/ipmi.pm
@@ -6517,6 +6517,9 @@ sub process_request {
 	  }
         }
       }
+
+      #donot update node provision status (installing or netbooting) here
+      xCAT::Utils->filter_nostatusupdate(\%newnodestatus);
       #print "newstatus" . Dumper(\%newnodestatus);
       xCAT_monitoring::monitorctrl::setNodeStatusAttributes(\%newnodestatus, 1);
     }
