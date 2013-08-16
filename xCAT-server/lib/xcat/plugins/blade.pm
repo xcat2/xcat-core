8eb4b81c9022abc43cbbee3dc67de96e2078891b
immarvin <yangsbj@cn.ibm.comq>
2013-08-16 01:38:09 -0700

    update node provision status based on the feedback of nodes instead of rpower/rnetboot

--- xCAT-server/lib/xcat/plugins/blade.pm
+++ xCAT-server/lib/xcat/plugins/blade.pm
@@ -5939,6 +5939,10 @@ sub dompa {
 	  }
         }
       }
+
+
+      #donot update node provision status (installing or netbooting) here
+      xCAT::Utils->filter_nostatusupdate(\%newnodestatus);
       #print "newstatus" . Dumper(\%newnodestatus);
       xCAT_monitoring::monitorctrl::setNodeStatusAttributes(\%newnodestatus, 1);
     }
