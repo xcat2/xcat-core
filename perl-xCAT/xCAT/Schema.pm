57db78797e6b6231a5b981289bed8f3e2ea5c611
lissav <lissav@us.ibm.com>
2013-09-18 14:24:16 -0400

    document xcatmaxbatchconnections update

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -1049,7 +1049,7 @@ site => {
    " xcatmaxconnections:  Number of concurrent xCAT protocol requests before requests\n".
    "                      begin queueing. This applies to both client command requests\n".
    "                      and node requests, e.g. to get postscripts. Default is 64.\n\n".
-   " xcatmaxbatchconnections:  Number of concurrent xCAT remote protocol requests allowed.\n".
+   " xcatmaxbatchconnections:  Number of concurrent xCAT connections allowed from the nodes.\n".
    "                      Value must be less than xcatmaxconnections. Default is 50.\n".
    " xcatdport:  The port used by the xcatd daemon for client/server communication.\n\n".
    " xcatiport:  The port used by xcatd to receive install status updates from nodes.\n\n",
