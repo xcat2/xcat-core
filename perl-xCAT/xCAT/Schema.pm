fa24d1f47c904a57d426f887089ae738e1541f6f
lissav <lissav@us.ibm.com>
2013-10-11 06:47:35 -0400

    defect 3819

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -1036,8 +1036,10 @@ site => {
    "           virtual network bridge up correctly. See\n".
    "           https://sourceforge.net/apps/mediawiki/xcat/index.php?title=XCAT_Virtualization_with_KVM#Setting_up_a_network_bridge\n\n".
    "               rsh/rcp will be setup and used on AIX. Default is yes.\n\n".
-   " useflowcontrol:  (yes/1 or no/0). If yes, postscripts will use xcatd flow control. If no,\n".
-   "               postscripts use wait and retry. Default is no.\n\n".
+   " useflowcontrol:  (yes/1 or no/0).If yes, postscripts use xcatd to control access to the server.\n".
+   "               If no, postscripts sleep and retry. On a new install, it will be set to yes.\n".
+   "               Works with xcatmaxconnections and xcatmaxbatch connections attributes.\n".
+   "               See the following documentation for details. https://sourceforge.net/apps/mediawiki/xcat/index.php?title=Hints_and_Tips_for_Large_Scale_Clusters\n\n".
    " useNFSv4onAIX:  (yes/1 or no/0). If yes, NFSv4 will be used with NIM. If no,\n".
    "               NFSv3 will be used with NIM. Default is no.\n\n".
    " vcenterautojoin:  When set to no, the VMWare plugin will not attempt to auto remove\n".
