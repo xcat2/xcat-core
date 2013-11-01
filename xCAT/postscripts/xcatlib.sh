f85ad98f0691ef847b25a86815d5d25715a85b08
xq2005 <qxuqxu@cn.ibm.com>
2013-11-01 03:34:35 -0700

    use ip address to find the network name

--- xCAT/postscripts/xcatlib.sh
+++ xCAT/postscripts/xcatlib.sh
@@ -71,3 +71,117 @@ function debianpreconf(){
     done
 }
 
+#tranfer the netmask to prefix for ipv4
+function v4mask2prefix(){
+    local num_bits=0
+    local old_ifs=$IFS
+    IFS=$'.'
+    local array_num_temp=($1)
+    IFS=$old_ifs
+    for num_dec in ${array_num_temp[@]}
+    do
+        case $num_dec in
+            255) let num_bits+=8;;
+            254) let num_bits+=7;;
+            252) let num_bits+=6;;
+            248) let num_bits+=5;;
+            240) let num_bits+=4;;
+            224) let num_bits+=3;;
+            192) let num_bits+=2;;
+            128) let num_bits+=1;;
+            0) ;;
+            *) echo "Error: $dec is not recognised"; exit 1
+        esac
+    done
+    echo "$num_bits"
+}
+
+function v4prefix2mask(){
+    local a=$1
+    local b=0
+    local num_index=1
+    local str_temp=''
+    local str_mask=''
+
+    while [[ $num_index -le 4 ]]
+    do
+        if [ $a -ge 8 ];then
+            b=8
+            a=$((a-8))
+        else
+            b=$a
+            a=0
+        fi
+        case $b in
+            0) str_temp="0";;
+            1) str_temp="128";;
+            2) str_temp="192";;
+            3) str_temp="224";;
+            4) str_temp="240";;
+            5) str_temp="248";;
+            6) str_temp="252";;
+            7) str_temp="254";;
+            8) str_temp="255";;
+        esac
+
+        str_mask=$str_mask$str_temp"."
+
+        num_index=$((num_index+1))
+    done
+
+    str_mask=`echo $str_mask | sed 's/.$//'`
+    echo "$str_mask"
+}
+
+function v4calcbcase(){
+    local str_mask=$2
+    echo $str_mask | grep '\.' > /dev/null
+    if [ $? -ne 0 ];then
+        str_mask=$(v4prefix2mask $str_mask)
+    fi
+    local str_bcast=''
+    local str_temp=''
+    local str_ifs=$IFS
+    IFS=$'.'
+    local array_ip=($1)
+    local array_mask=($str_mask)
+    IFS=$str_ifs
+
+    if [ ${#array_ip[*]} -ne 4 -o ${#array_mask[*]} -ne 4 ];then
+        echo "255.255.255.255"
+        return
+    fi
+
+    for index in {0..3}
+    do
+        str_temp=`echo $[ ${array_ip[$index]}|(${array_mask[$index]} ^ 255) ]`
+        str_bcast=$str_bcast$str_temp"."
+    done
+
+    str_bcast=`echo $str_bcast | sed 's/.$//'`
+    echo "$str_bcast"
+}
+
+function v4calcnet(){
+    local str_mask=$2
+    echo $str_mask | grep '\.' > /dev/null
+    if [ $? -ne 0 ];then
+        str_mask=$(v4prefix2mask $str_mask)
+    fi
+    local str_net=''
+    local str_temp=''
+    local str_ifs=$IFS
+    IFS=$'.'
+    local array_ip=($1)
+    local array_mask=($str_mask)
+    IFS=$str_ifs
+
+    for index in {0..3}
+    do
+        str_temp=`echo $[ ${array_ip[$index]}&${array_mask[$index]} ]`
+        str_net=$str_net$str_temp"."
+    done
+
+    str_net=`echo $str_net | sed 's/.$//'`
+    echo "$str_net"
+}
