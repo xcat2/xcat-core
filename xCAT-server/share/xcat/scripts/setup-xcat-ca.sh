6e7009e9e6c40cd4944415615499afa089918087
Jarrod Johnson <jbjohnso@us.ibm.com>
2014-04-11 14:13:08 -0400

    Backdate the CA certificate itself

--- xCAT-server/share/xcat/scripts/setup-xcat-ca.sh
+++ xCAT-server/share/xcat/scripts/setup-xcat-ca.sh
@@ -30,5 +30,9 @@ sed -e "s@##XCATCADIR##@$XCATCADIR@" $XCATROOT/share/xcat/ca/openssl.cnf.tmpl > 
 cp $XCATROOT/share/xcat/ca/Makefile $XCATCADIR/
 cd $XCATCADIR
 make init
-openssl req -nodes -config openssl.cnf -days 7300 -x509 -newkey rsa:2048 -out ca-cert.pem -extensions v3_ca -outform PEM -subj /CN="$CNA"
+#openssl req -nodes -config openssl.cnf -days 7300 -x509 -newkey rsa:2048 -out ca-cert.pem -extensions v3_ca -outform PEM -subj /CN="$CNA"
+openssl genrsa -out private/ca-key.pem 2048
+chmod 600 private/ca-key.pem
+openssl req -new -key private/ca-key.pem -config openssl.cnf -out ca-req.csr -subj /CN="$CNA" -outform PEM
+openssl ca -selfsign -keyfile private/ca-key.pem -in ca-req.csr -startdate 19700101010101Z -days 7305 -extensions v3_ca -config openssl.cnf -out ca-cert.pem
 cd -
