xcat2/xcat-core

commit 899cb5028ddfa672e9af9ba884316072ef529048

defect 4147 - allow nodeset to continue if prescript errors

xCAT-server/lib/xcat/plugins/yaboot.pm
@@ -469,9 +469,8 @@ sub process_request {
       if ($errored) {
 	  my $rsp;
  	  $rsp->{errorcode}->[0]=1;
-	  $rsp->{error}->[0]="Failed in running begin prescripts.\n";
+	  $rsp->{error}->[0]="Failed in running begin prescripts.  Processing will still continue.\n";
 	  $::YABOOT_callback->($rsp);
-	  return; 
       }
   } 
 
@@ -726,9 +725,14 @@ sub process_request {
       if ($errored) { 
 	  my $rsp;
 	  $rsp->{errorcode}->[0]=1;
+<<<<<<< HEAD
 	  $rsp->{error}->[0]="Failed in running end prescripts\n";
 	  $::YABOOT_callback->($rsp);
 	  return; 
+=======
+	  $rsp->{error}->[0]="Failed in running end prescripts.  Processing will still continue.\n";
+	  $callback->($rsp);
+>>>>>>> 78b8d21... defect 4147 - allow nodeset to continue if prescript errors
       }
   }
 }

xCAT-server/lib/xcat/plugins/pxe.pm
@@ -414,9 +414,8 @@ sub process_request {
        if ($errored) { 
 	  my $rsp;
 	  $rsp->{errorcode}->[0]=1;
-	  $rsp->{error}->[0]="Failed in running begin prescripts\n";
+	  $rsp->{error}->[0]="Failed in running begin prescripts.  Processing will still continue.\n";
 	  $::PXE_callback->($rsp);
-	  return; 
        }
    }
   
@@ -577,9 +576,8 @@ sub process_request {
       if ($errored) { 
 	  my $rsp;
 	  $rsp->{errorcode}->[0]=1;
-	  $rsp->{error}->[0]="Failed in running end prescripts\n";
+	  $rsp->{error}->[0]="Failed in running end prescripts.  Processing will still continue.\n";
 	  $::PXE_callback->($rsp);
-	  return; 
       }
   }
 

xCAT-server/lib/xcat/plugins/vsmppxe.pm
@@ -349,9 +349,8 @@ sub process_request {
        if ($errored) { 
 	  my $rsp;
 	  $rsp->{errorcode}->[0]=1;
-	  $rsp->{error}->[0]="Failed in running begin prescripts\n";
+	  $rsp->{error}->[0]="Failed in running begin prescripts.  Processing will still continue.\n";
 	  $::VSMPPXE_callback->($rsp);
-	  return; 
        }
    }
   
@@ -466,9 +465,8 @@ sub process_request {
       if ($errored) {
 	  my $rsp;
 	  $rsp->{errorcode}->[0]=1;
-	  $rsp->{error}->[0]="Failed in running end prescripts\n";
+	  $rsp->{error}->[0]="Failed in running end prescripts.  Processing will still continue.\n";
 	  $::VSMPPXE_callback->($rsp);
-	  return; 
       }
   }
 

xCAT-server/lib/xcat/plugins/xnba.pm
@@ -435,9 +435,8 @@ sub process_request {
       if ($errored) { 
 	  my $rsp;
  	  $rsp->{errorcode}->[0]=1;
-	  $rsp->{error}->[0]="Failed in running begin prescripts.\n";
+	  $rsp->{error}->[0]="Failed in running begin prescripts.  Processing will still continue.\n";
 	  $::XNBA_callback->($rsp);
-	  return; 
       }
   }  
 
@@ -564,9 +563,8 @@ sub process_request {
       if ($errored) { 
 	  my $rsp;
  	  $rsp->{errorcode}->[0]=1;
-	  $rsp->{error}->[0]="Failed in running end prescripts.\n";
+	  $rsp->{error}->[0]="Failed in running end prescripts.  Processing will still continue.\n";
 	  $::XNBA_callback->($rsp);
-	  return; 
       }
   }
 

commit 1b9e2b16cfd0f8df7fa10d86d415d90fa02e21e7

defect 4153: resolve the merge conflict which introduced by commit 899cb5028ddfa672e9af9ba884316072ef529048

xCAT-server/lib/xcat/plugins/yaboot.pm
@@ -725,14 +725,8 @@ sub process_request {
       if ($errored) { 
 	  my $rsp;
 	  $rsp->{errorcode}->[0]=1;
-<<<<<<< HEAD
-	  $rsp->{error}->[0]="Failed in running end prescripts\n";
-	  $::YABOOT_callback->($rsp);
-	  return; 
-=======
 	  $rsp->{error}->[0]="Failed in running end prescripts.  Processing will still continue.\n";
-	  $callback->($rsp);
->>>>>>> 78b8d21... defect 4147 - allow nodeset to continue if prescript errors
+	  $::YABOOT_callback->($rsp);
       }
   }
 }