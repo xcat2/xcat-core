69e6dd69cfd656c27f4d7ea8c52faf89df26d835
immarvin <yangsbj@cn.ibm.com>
2014-06-09 06:40:14 -0700

    repleace the servcie stuff with newly added generic subroutines in xcat-client

--- perl-xCAT/xCAT/Utils.pm
+++ perl-xCAT/xCAT/Utils.pm
@@ -3584,6 +3584,10 @@ sub servicemap{
                  0=>["ntpd","ntp"],
                  1=>["ntpd.service"],
                },
+     "mysql" =>  {
+                 0=>["mysqld","mysql"],
+                 1=>["mysqld.service"],
+               },
   );
 
   my $path=undef;
