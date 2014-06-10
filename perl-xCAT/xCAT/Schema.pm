f5851bc826b6c780ceac3f5a5dfab2755cbbd91e
Jarrod Johnson <jbjohnso@us.ibm.com>
2014-06-10 14:06:27 -0400

    Elaborate upon ipmi.bmcport behavior for a number of IBM system x systems

--- perl-xCAT/xCAT/Schema.pm
+++ perl-xCAT/xCAT/Schema.pm
@@ -393,7 +393,46 @@ ipmi => {
  descriptions => {
   node => 'The node name or group name.',
   bmc => 'The hostname of the BMC adapater.',
-  bmcport => 'In systems with selectable shared/dedicated ethernet ports, this parameter can be used to specify the preferred port.  0 means use the shared port, 1 means dedicated, blank is to not assign',
+  bmcport => ' In systems with selectable shared/dedicated ethernet ports,
+           this parameter can be used to specify the preferred port.  0
+           means use the shared port, 1 means dedicated, blank is to not
+           assign.
+
+           The following special cases exist for IBM System x servers:
+
+           For x3755 M3 systems, 0 means use the dedicated port, 1 means
+           shared, blank is to not assign.
+
+       For certain systems which have a mezzaine or ML2 adapter, there is a second
+       value to include:
+
+
+           For x3750 M4 (Model 8722):
+
+
+           0 2   1st 1Gbps interface for LOM
+
+           0 0   1st 10Gbps interface for LOM
+
+           0 3   2nd 1Gbps interface for LOM
+
+           0 1   2nd 10Gbps interface for LOM
+
+
+           For  x3750 M4 (Model 8752), x3850/3950 X6, dx360 M4, x3550 M4, and x3650 M4:
+
+
+           0     Shared (1st onboard interface)
+
+           1     Dedicated
+
+           2 0   First interface on ML2 or mezzanine adapter
+
+           2 1   Second interface on ML2 or mezzanine adapter
+
+           2 2   Third interface on ML2 or mezzanine adapter
+
+           2 3   Fourth interface on ML2 or mezzanine adapter',
   taggedvlan => 'Have bmcsetup place the BMC on the specified vlan tag on a shared netwirk interface.  Some network devices may be incompatible with this option',
   bmcid => 'Unique identified data used by discovery processes to distinguish known BMCs from unrecognized BMCs',
   username => 'The BMC userid.  If not specified, the key=ipmi row in the passwd table is used as the default.',
