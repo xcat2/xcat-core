b9577cb9ef96e5b1c4af8cc0510d3d5c65525214
baiyuan <bybai@cn.ibm.com>
2014-10-17 03:22:36 -0400

    non_native package use this script, update sample using real content

--- xCAT-buildkit/share/xcat/kits/kit_template/scripts/sample/genimage_post.sh
+++ xCAT-buildkit/share/xcat/kits/kit_template/scripts/sample/genimage_post.sh
@@ -1,3 +1,21 @@
+#!/bin/sh
 
-echo "running sample-comp1 genimage_post script"
+rpmdir="/opt/xcat/kits/<<<buildkit_WILL_INSERT_kit_basename_HERE>>>/<<<buildkit_WILL_INSERT_kitcomponent_name_HERE>>>"
 
+if [[ ! -z "$installroot" ]]; then
+    if [ -n "`ls $installroot$rpmdir/*.deb 2> /dev/null`" ] ; then
+        dpkg -i --force-all --instdir=$installroot  $installroot$rpmdir/*.deb
+   
+    elif [ -n "`ls $installroot$rpmdir/*.rpm 2> /dev/null`" ] ; then
+        rpm --force --root $installroot -Uvh $installroot$rpmdir/*.rpm
+    fi
+else
+    if [ -n "`ls $rpmdir/*.deb 2> /dev/null`" ] ; then
+        dpkg -i --force-all $rpmdir/*.deb
+    
+    elif [ -n "`ls $rpmdir/*.rpm 2> /dev/null`" ] ; then
+        rpm --force -Uvh $rpmdir/*.rpm
+    fi
+fi
+
+exit 0
