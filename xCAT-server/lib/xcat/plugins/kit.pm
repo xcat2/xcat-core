c51cfca5e1c98ce3a1be7abdd68078932652baef
chudegao <degaochu@cn.ibm.com>
2015-03-11 18:02:31 +0800

    fix feature request 189, support not contain existing postscript in kit

--- xCAT-server/lib/xcat/plugins/kit.pm
+++ xCAT-server/lib/xcat/plugins/kit.pm
@@ -1346,14 +1346,14 @@ sub addkit
             } else {
                 $rc = system("cp -rf $kitdir/other_files/$_ $installdir/postscripts/");
             }
+            if($rc && !-e "$installdir/postscripts/$_"){
+                my %rsp;
+                push@{ $rsp{data} }, "Failed to copy scripts from $kitdir/scripts/ to $installdir/postscripts";
+                xCAT::MsgUtils->message( "E", \%rsp, $callback );
+                return 1;
+            }
+            $rc = 0;
             chmod(0755,"$installdir/postscripts/$_");
-        }
-
-        if($rc){
-            my %rsp;
-            push@{ $rsp{data} }, "Failed to copy scripts from $kitdir/scripts/ to $installdir/postscripts";
-            xCAT::MsgUtils->message( "E", \%rsp, $callback );
-            return 1;
         }
 
         # Copying plugins to /opt/xcat/lib/perl/xCAT_plugin/
