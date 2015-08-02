32267bc6b652ac73e9d3f9b006532e0fac5b3b20
yangsong <yangsbj@cn.ibm.com>
2015-08-02 04:31:43 -0400

    enable syslog in the stateless initrd, add logs in the xcat dracut module for sles

---
+++ xCAT-server/share/xcat/netboot/sles/dracut_033/patch/rsyslogd-start.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+# Triggered by udev and starts rsyslogd with bootparameters
+
+type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
+
+rsyslog_config() {
+    local server=$1
+    shift
+    local syslog_template=$1
+    shift
+    local filters=$*
+    local filter=
+    local confline=  
+
+    cat $syslog_template
+
+#    for filter in $filters; do
+#        echo "${filter} @${server}"
+#    done
+
+    
+    if [ -n "$filters" ];then
+        confline="${filters}";
+    else
+        confline="*.*"
+    fi
+
+    if [ -n "$server" ];then
+       confline="$confline @${server}"
+    else
+       confline="$confline /var/log/messages"
+    fi
+
+    echo "$confline"
+#       echo "*.* /tmp/syslog"
+}
+
+read server < /tmp/syslog.server
+read filters < /tmp/syslog.filters
+[ -z "$filters" ] && filters="kern.*"
+read conf < /tmp/syslog.conf
+[ -z "$conf" ] && conf="/etc/rsyslog.conf" && echo "$conf" > /tmp/syslog.conf
+
+template=/etc/templates/rsyslog.conf
+if [ -n "$server" ]; then
+    rsyslog_config "$server" "$template" "$filters" > $conf
+    rsyslogd -i /var/run/syslogd.pid 
+fi
