b66b3180bf016ae1f59e134408a0e7ab212de62a
chenglch <chenglch@cn.ibm.com>
2016-03-16 02:22:32 -0400

    temperary pertidy

--- test/performance/case/test_dbobject.pl
+++ test/performance/case/test_dbobject.pl
@@ -11,13 +11,13 @@ use xCAT::Utils;
 use Time::HiRes qw/time/;
 use File::Copy;
 
-use constant CACHE_XCATD_PATH  => '/opt/xcat/sbin/xcatd.cache';
-use constant NOCACHE_XCATD_PATH  => '/opt/xcat/sbin/xcatd.nocache';
-use constant DEST_XCATD_PATH => '/opt/xcat/sbin/xcatd';
+use constant CACHE_XCATD_PATH   => '/opt/xcat/sbin/xcatd.cache';
+use constant NOCACHE_XCATD_PATH => '/opt/xcat/sbin/xcatd.nocache';
+use constant DEST_XCATD_PATH    => '/opt/xcat/sbin/xcatd';
 
-use constant CACHE_TABLE_PATH => '/opt/xcat/lib/perl/xCAT/Table.pm.cache';
+use constant CACHE_TABLE_PATH   => '/opt/xcat/lib/perl/xCAT/Table.pm.cache';
 use constant NOCACHE_TABLE_PATH => '/opt/xcat/lib/perl/xCAT/Table.pm.nocache';
-use constant DEST_TABLE_PATH => '/opt/xcat/lib/perl/xCAT/Table.pm';
+use constant DEST_TABLE_PATH    => '/opt/xcat/lib/perl/xCAT/Table.pm';
 
 my $number = $ARGV[0];
 
@@ -26,89 +26,90 @@ sub get_subroutine_name {
 }
 
 sub setup_nocache_xcat {
-	copy(NOCACHE_XCATD_PATH,DEST_XCATD_PATH) or die "Copy failed: $!";
-	copy(NOCACHE_TABLE_PATH,DEST_TABLE_PATH) or die "Copy failed: $!";	
+    copy(NOCACHE_XCATD_PATH, DEST_XCATD_PATH) or die "Copy failed: $!";
+    copy(NOCACHE_TABLE_PATH, DEST_TABLE_PATH) or die "Copy failed: $!";
 }
 
 sub setup_cache_xcat {
-	copy(CACHE_XCATD_PATH,DEST_XCATD_PATH) or die "Copy failed: $!";
-    copy(CACHE_TABLE_PATH,DEST_TABLE_PATH) or die "Copy failed: $!";  	
+    copy(CACHE_XCATD_PATH, DEST_XCATD_PATH) or die "Copy failed: $!";
+    copy(CACHE_TABLE_PATH, DEST_TABLE_PATH) or die "Copy failed: $!";
 }
 
 sub restart_xcat_service {
-	xCAT::Utils->runcmd("service xcatd restart ", -1);
-	if ($::RUNCMD_RC != 0) {
+    xCAT::Utils->runcmd("service xcatd restart ", -1);
+    if ($::RUNCMD_RC != 0) {
         die "Failed to restart xcatd service";
     }
 }
 
 sub test_xcat_with_cache {
-	setup_cache_xcat();
-	restart_xcat_service();
-	prepare_data($number);	
+    setup_cache_xcat();
+    restart_xcat_service();
+    prepare_data($number);
 }
 
 sub test_xcat_without_cache {
-	setup_nocache_xcat();
-	restart_xcat_service();
-	prepare_data($number);	
+    setup_nocache_xcat();
+    restart_xcat_service();
+    prepare_data($number);
 }
 
 
 sub cleanup_data {
-   my $count = shift;
-   if(!defined($count)) {
-       $count = 100;
-   }
-   xCAT::Utils->runcmd("rmdef node[1-$count]", -1);
-   if ($::RUNCMD_RC != 0) {
+    my $count = shift;
+    if (!defined($count)) {
+        $count = 100;
+    }
+    xCAT::Utils->runcmd("rmdef node[1-$count]", -1);
+    if ($::RUNCMD_RC != 0) {
         die "Failed to destroy data";
-   }
+    }
 }
+
 sub prepare_data {
-	my $count = shift;
-	if(!defined($count)) {
-		$count = 100;
-	}
-	xCAT::Utils->runcmd("chdef node[1-$count] mgt=ipmi groups=all arch=x86_64 os=rhels7.0"
-	                   ." nodetype=osi profile=service netboot=kvm installnic=mac"
-	                   ." primarynic=mac provmethod=rhels7-x86_64-install-service"
-	                   ." bmcpassword=abc123 bmcusername=ADMIN ", -1);
+    my $count = shift;
+    if (!defined($count)) {
+        $count = 100;
+    }
+    xCAT::Utils->runcmd("chdef node[1-$count] mgt=ipmi groups=all arch=x86_64 os=rhels7.0"
+          . " nodetype=osi profile=service netboot=kvm installnic=mac"
+          . " primarynic=mac provmethod=rhels7-x86_64-install-service"
+          . " bmcpassword=abc123 bmcusername=ADMIN ", -1);
     if ($::RUNCMD_RC != 0) {
         die "Failed to create data";
     }
 }
 
 sub test_lsdef {
-	my $count = shift;
-	if (!defined($count)) {
-		$count = 1;
-	}
-	for(my $i=0; $i<$count; $i++) {
-		xCAT::Utils->runcmd("lsdef", -1);
+    my $count = shift;
+    if (!defined($count)) {
+        $count = 1;
+    }
+    for (my $i = 0 ; $i < $count ; $i++) {
+        xCAT::Utils->runcmd("lsdef", -1);
         if ($::RUNCMD_RC != 0) {
             die "Failed run lsdef command";
         }
-	}
-    return get_subroutine_name()."_$count";
+    }
+    return get_subroutine_name() . "_$count";
 }
 
 sub test_lsdef_node {
-	my $count = shift;
-	if (!defined($count)) {
+    my $count = shift;
+    if (!defined($count)) {
         $count = 1;
     }
-	for(my $i=0; $i<$count; $i++) {
-	    xCAT::Utils->runcmd("lsdef node10", -1);
-	    if ($::RUNCMD_RC != 0) {
-	        die "Failed run lsdef node command";
-	    }
-	}	
-    return get_subroutine_name()."_$count";
+    for (my $i = 0 ; $i < $count ; $i++) {
+        xCAT::Utils->runcmd("lsdef node10", -1);
+        if ($::RUNCMD_RC != 0) {
+            die "Failed run lsdef node command";
+        }
+    }
+    return get_subroutine_name() . "_$count";
 }
 
 sub test_dbobject {
-    my $obj_func    = shift;
+    my $obj_func   = shift;
     my $params_ptr = shift;
     my $start_time = time();
     my $subroutine;
@@ -127,25 +128,25 @@ sub test_dbobject {
 }
 
 sub main() {
-	print "\nTesting with cache version\n";
-	test_xcat_with_cache();
-	test_dbobject(\&test_lsdef);
-	test_dbobject(\&test_lsdef_node);
-	test_dbobject(\&test_lsdef);
+    print "\nTesting with cache version\n";
+    test_xcat_with_cache();
+    test_dbobject(\&test_lsdef);
     test_dbobject(\&test_lsdef_node);
-    test_dbobject(\&test_lsdef, 10);
+    test_dbobject(\&test_lsdef);
+    test_dbobject(\&test_lsdef_node);
+    test_dbobject(\&test_lsdef,      10);
     test_dbobject(\&test_lsdef_node, 10);
-    
+
     print "\nNow testing with no cache version\n";
     test_xcat_without_cache();
     test_dbobject(\&test_lsdef);
     test_dbobject(\&test_lsdef_node);
     test_dbobject(\&test_lsdef);
     test_dbobject(\&test_lsdef_node);
-    
-    test_dbobject(\&test_lsdef,10);
+
+    test_dbobject(\&test_lsdef,      10);
     test_dbobject(\&test_lsdef_node, 10);
-    
+
     print "\nClean up data\n";
     cleanup_data($number);
 }
