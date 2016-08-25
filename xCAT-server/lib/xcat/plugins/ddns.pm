12773c44fc9b48c8615e8f6352c1f0e52c815b65
bybai <bybai@cn.ibm.com>
2016-08-25 01:52:19 -0400

    fix issue 1553 makedns failed when input invalid hostname into /etc/hosts

--- xCAT-server/lib/xcat/plugins/ddns.pm
+++ xCAT-server/lib/xcat/plugins/ddns.pm
@@ -423,6 +423,8 @@ sub process_request {
         my $names;
         my @hosts;
         my %nodehash;
+        my @eachhost;
+        my $invalid;
 
         foreach (@contents) {
             chomp;            #no newline
@@ -437,6 +439,18 @@ sub process_request {
             }
             unless ($names =~ /^[a-z0-9\. \t\n-]+$/i) {
                 xCAT::SvrUtils::sendmsg(":Ignoring line $_ in /etc/hosts, names  $names contain invalid characters (valid characters include a through z, numbers and the '-', but not '_'", $callback);
+                next;
+            }
+
+            @eachhost = split(/ /,$names);
+            foreach my $hname (@eachhost) {
+                unless ($hname !~ /^\.[a-z0-9]+/i) {
+                    xCAT::SvrUtils::sendmsg(":Ignoring line $_ in /etc/hosts, names  $names start with . ", $callback);
+                    $invalid = $names;
+                    last;
+                }
+            }
+            if ($invalid) {
                 next;
             }
 
