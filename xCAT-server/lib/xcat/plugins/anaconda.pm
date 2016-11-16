5fa989624c5db322035f177579ad13579832ecec
penguhyang <ypbj@cn.ibm.com>
2016-11-16 00:08:17 -0500

    fix copycds can't create statelite osimage

--- xCAT-server/lib/xcat/plugins/anaconda.pm
+++ xCAT-server/lib/xcat/plugins/anaconda.pm
@@ -2492,6 +2492,7 @@ sub copycd
 
             my @ret = xCAT::SvrUtils->update_tables_with_diskless_image($distname, $arch, undef, "netboot", $path, $osdistroname);
 
+            my @ret = xCAT::SvrUtils->update_tables_with_diskless_image($distname, $arch, undef, "statelite", $path, $osdistroname);
             #if ($ret[0] != 0) {
             #$callback->({data => "Error when updating the osimage tables for stateless: " . $ret[1]});
             #}
