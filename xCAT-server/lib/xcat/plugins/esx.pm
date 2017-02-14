a613bd2ac88a1c17f81d6d16f4b6c5e409b7b3d9
Casandra Qiu <cxhong@us.ibm.com>
2017-02-14 14:08:09 -0500

    Add PDU documentation

--- xCAT-server/lib/xcat/plugins/esx.pm
+++ xCAT-server/lib/xcat/plugins/esx.pm
@@ -407,7 +407,7 @@ sub process_request {
     }
 
     #pdu commands will be handled in the pdu plugin
-    if ($command eq "rpower" and grep(/^pduon|pduoff|pdustat/, @exargs)) {
+    if ($command eq "rpower" and grep(/^pduon|pduoff|pdustat$/, @exargs)) {
         return;
     }
     #my $sitetab = xCAT::Table->new('site');
