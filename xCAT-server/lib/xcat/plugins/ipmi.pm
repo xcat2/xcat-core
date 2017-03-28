2ac5da13efc80d0ad5e3783c43944cfb6a2acacf
Jarrod Johnson <jjohnson2@lenovo.com>
2017-03-28 11:11:36 -0400

    Fix missing sessdata

--- xCAT-server/lib/xcat/plugins/ipmi.pm
+++ xCAT-server/lib/xcat/plugins/ipmi.pm
@@ -2072,6 +2072,7 @@ sub abort_fpc_update {
 }
 
 sub fpc_update_aborted {
+    my $sessdata = $_[1];
     if ($_[0]->{code}) {
         xCAT::SvrUtils::sendmsg("Unexpected condition, restarting device", $callback, $sessdata->{node}, %allerrornodes);
         $sessdata->{ipmisession}->subcmd(netfn => 0x2e, command => 0xcc, data => [0x2e, 0xcc, 0x5e, 0x2b, 0, 6, 0x43, 0], callback => \&fpc_update_aborted, callback_args => $sessdata);
