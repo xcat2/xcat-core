e1c00a5d037f01698b89e4ee2a0391c02c64cf92
DengShuaiSimon <denfshuaishuai@icloud.com>
2017-06-27 12:16:31 +0800

    Update travis.rb

--- travis.rb
+++ travis.rb
@@ -318,7 +318,7 @@ if(event_type == "pull_request")
   system("sudo chmod 777 /etc/apt/sources.list")
   system('sudo echo "deb [arch=amd64] http://xcat.org/files/xcat/repos/apt/xcat-dep trusty main" >> /etc/apt/sources.list')
   system('sudo echo "deb [arch=ppc64el] http://xcat.org/files/xcat/repos/apt/xcat-dep trusty main" >> /etc/apt/sources.list')
-  #system("sudo cat /etc/apt/sources.list")
+  system("sudo cat /etc/apt/sources.list")
  
   
   `sudo apt-get clean all`
@@ -326,7 +326,7 @@ if(event_type == "pull_request")
   system("sudo apt-get -qq update")
   ##`sudo apt-get install xCAT --force-yes -y`
   puts "\033[42m sudo apt-get install xcat --force-yes \033[0m\n"
-  installresult = system("sudo apt-get install xCAT --force-yes >/tmp/install-log 2>&1")
+  installresult = system("sudo apt-get install xcat --force-yes >/tmp/install-log 2>&1")
   puts "installresult : #{installresult}"
   system("cat /tmp/install-log")
   if(!installresult)
