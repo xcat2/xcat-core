beeb80cfdeb201f00e8e53286802a1fef680790c
bybai <bybai@cn.ibm.com>
2017-08-27 22:32:35 -0400

    polished based on comments.

--- xCAT/postscripts/nicutils.sh
+++ xCAT/postscripts/nicutils.sh
@@ -1473,7 +1473,7 @@ function create_bond_interface {
     wait_for_ifstate $ifname UP 200 1
     rc=$?
     if [ $rc -ne 0 ]; then
-        log_error "Interface \"$ifname\" could not be brought to \"UP\"."
+        log_error "Interface \"$ifname\" could not be brought \"UP\"."
         $ip link show $ifname \
         | $sed -e 's/^/['$ifname' ip out >> /g' \
         | log_lines info
