f2ff7cdb73caadc61c30129630f7f1359104e43b
Victor Hu <whowutwut@gmail.com>
2017-08-30 14:21:14 -0400

    Add usage change for rspconfig to set hostname on OpenBMC

--- perl-xCAT/xCAT/Usage.pm
+++ perl-xCAT/xCAT/Usage.pm
@@ -143,7 +143,7 @@ my %usage = (
        rspconfig <noderange> [garp=<number of 1/2 second>]
        rspconfig <noderange> [userid=<userid> username=<username> password=<password>]
    OpenBMC specific:
-       rspconfig <noderange> [ip|netmask|gateway|vlan]
+       rspconfig <noderange> [ip|netmask|gateway|hostname|vlan]
    iDataplex specific:
        rspconfig <noderange> [thermprofile]
        rspconfig <noderange> [thermprofile=<two digit number from chassis>]
