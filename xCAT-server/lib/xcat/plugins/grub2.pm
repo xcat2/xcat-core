ad564148c75f51c6788b26ab2d1e47c1c26dc898
Bin Xu <bxuxa@cn.ibm.com>
2017-09-19 10:01:23 +0800

    Fix 3930, 1, only re-distribute to accessible dhcpsrv; 2, for non-sharedtftp mode, run makedhcp -d -l for offline

--- xCAT-server/lib/xcat/plugins/grub2.pm
+++ xCAT-server/lib/xcat/plugins/grub2.pm
@@ -504,8 +504,7 @@ sub preprocess_request {
             if ($ntab) {
                 foreach (@{ $ntab->getAllEntries() }) {
                     next unless ($_->{dynamicrange});
-                    # if dynamicrange specified but dhcpserver was not - issue error message
-                    push @dhcpsvrs, $_->{dhcpserver} if ($_->{dhcpserver})
+                    push @dhcpsvrs, $_->{dhcpserver} if ($_->{dhcpserver} && xCAT::NetworkUtils->nodeonmynet($_->{dhcpserver}));
                 }
             }
             return xCAT::Scope->get_broadcast_disjoint_scope_with_parallel($req, $sn_hash, \@dhcpsvrs);
@@ -854,7 +853,11 @@ sub process_request {
                 push(@rmdhcp_nodes, $tmp_node);
             }
         }
-        $sub_req->({ command => ['makedhcp'], arg => ['-d'], node => \@rmdhcp_nodes }, $callback);
+        if ($request->{'_disparatetftp'}->[0]) {
+            $sub_req->({ command => ['makedhcp'], arg => ['-d', '-l'], node => \@rmdhcp_nodes }, $callback);
+        } else {
+            $sub_req->({ command => ['makedhcp'], arg => ['-d'], node => \@rmdhcp_nodes }, $callback);
+        }
     }
 
     #now run the end part of the prescripts
