826dcd50b8b5abfc9121db952b4274a1b7bacb52
GONG Jie <gongjie@linux.vnet.ibm.com>
2017-12-31 23:59:59 +0000

    Remove trailing spaces in file xCAT-buildkit/share/xcat/kits/kit_template/scripts/sample/genimage_post.sh

--- xCAT-buildkit/share/xcat/kits/kit_template/scripts/sample/genimage_post.sh
+++ xCAT-buildkit/share/xcat/kits/kit_template/scripts/sample/genimage_post.sh
@@ -5,14 +5,14 @@ rpmdir="/opt/xcat/kits/<<<buildkit_WILL_INSERT_kit_basename_HERE>>>/<<<buildkit_
 if [[ ! -z "$installroot" ]]; then
     if [ -n "`ls $installroot$rpmdir/*.deb 2> /dev/null`" ] ; then
         dpkg -i --force-all --instdir=$installroot  $installroot$rpmdir/*.deb
-   
+
     elif [ -n "`ls $installroot$rpmdir/*.rpm 2> /dev/null`" ] ; then
         rpm --force --root $installroot -Uvh $installroot$rpmdir/*.rpm
     fi
 else
     if [ -n "`ls $rpmdir/*.deb 2> /dev/null`" ] ; then
         dpkg -i --force-all $rpmdir/*.deb
-    
+
     elif [ -n "`ls $rpmdir/*.rpm 2> /dev/null`" ] ; then
         rpm --force -Uvh $rpmdir/*.rpm
     fi
