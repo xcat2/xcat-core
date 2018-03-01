429c5d1d035feddba8f5930fd78716fab032e1af
Casandra Qiu <cxhong@us.ibm.com>
2018-03-01 16:15:47 -0500

    modified from reviews

--- xCAT-server/lib/xcat/plugins/switchdiscover.pm
+++ xCAT-server/lib/xcat/plugins/switchdiscover.pm
@@ -1491,11 +1491,11 @@ sub switchsetup {
         if (-r -x $config_script) {
             my $switches = join(",",@{${nodes_to_config}->{$mytype}});
             if ($mytype eq "onie") {
-                send_msg($request, 0, "Call $config_script to config $switches\n");
+                send_msg($request, 0, "Calling $config_script to configure $mytype switches  $switches...\n");
                 my $out = `$config_script --switches $switches --all`;
                 send_msg($request, 0, "output = $out\n");
             } else {
-                send_msg($request, 0, "call $config_script to config $mytype switches $switches\n");
+                send_msg($request, 0, "Calling $config_script to configure $mytype switches $switches...\n");
                 if ($mytype =~ /Mellanox/) {
                     send_msg($request, 0, "NOTE: If command takes too long for Mellanox IB switch, open another window and ping the switches being configured, once they are complete, ctrl-c out of this command\n");
                     send_msg($request, 0, "then run $config_script --switches $switches --all again\n");
