8c5701d9ab223ec2805abc4c5ee2a6c0df47a65f
Mark Gurevich <gurevich@us.ibm.com>
2018-03-28 14:36:58 -0400

    OpenBMC reventlog flag verification

--- xCAT-server/lib/xcat/plugins/openbmc2.pm
+++ xCAT-server/lib/xcat/plugins/openbmc2.pm
@@ -364,7 +364,12 @@ sub parse_args {
         }
     } elsif ($command eq "reventlog") {
         $subcommand = "all" if (!defined($ARGV[0]));
-        if ($subcommand =~ /^resolved=(.*)/) {
+        if (scalar(@ARGV) >= 2) {
+            if ($ARGV[1] =~ /^-s$/) {
+                return ([ 1, "The -s option is not supported for OpenBMC." ]);
+            }
+            return ([ 1, "Only one option is supported at the same time for $command" ]);
+        } elsif ($subcommand =~ /^resolved=(.*)/) {
             my $value = $1;
             if (not $value) {
                 return ([ 1, "$usage_errormsg $reventlog_no_id_resolved_errormsg" ]);
