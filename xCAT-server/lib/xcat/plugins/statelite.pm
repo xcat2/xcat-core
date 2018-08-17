dcaa21aa13d04c3b53fd6b2e98b3691991220543
cxhong <abcdqiu@gmail.com>
2018-08-16 23:01:55 -0400

    Support multiple syncfile list for packimage process (#5524)

--- xCAT-server/lib/xcat/plugins/statelite.pm
+++ xCAT-server/lib/xcat/plugins/statelite.pm
@@ -233,10 +233,14 @@ sub process_request {
 
     #sync fils configured in the synclist to the rootimage
     $syncfile = xCAT::SvrUtils->getsynclistfile(undef, $osver, $arch, $profile, "netboot", $imagename);
-    if (defined($syncfile) && -f $syncfile
-        && -d $rootimg_dir) {
-        print "sync files from $syncfile to the $rootimg_dir\n";
-        `$::XCATROOT/bin/xdcp -i $rootimg_dir -F $syncfile`;
+    if (defined($syncfile) && -d $rootimg_dir) {
+        my @filelist = split ',', $syncfile;
+        foreach my $synclistfile (@filelist) {
+            if ( -f $synclistfile) {
+                print "sync files from $synclistfile to the $rootimg_dir\n";
+                `$::XCATROOT/bin/xdcp -i $rootimg_dir -F $synclistfile`;
+            }
+        }
     }
 
     # check if the file "litefile.save" exists or not
