319a9906e44a34394be52ac5452c521f9eeb3b3a
GONG Jie <gongjie@linux.vnet.ibm.com>
2018-08-29 15:20:47 +0800

    Subdirectory dracut_047 for RHEL8 support

---
+++ xCAT-server/share/xcat/netboot/rh/dracut_047/xcat-premount.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#script to update nodelist.nodestatus during provision
+
+XCAT="$(getarg XCAT=)"
+STATEMNT="$(getarg STATEMNT=)"
+MASTER=`echo $XCAT |awk -F: '{print $1}'`
+
+getarg nonodestatus
+NODESTATUS=$?
+
+XCATIPORT="$(getarg XCATIPORT=)"
+if [ $? -ne 0 ]; then
+XCATIPORT="3002"
+fi
+
+
+
+if [ $NODESTATUS -ne 0 ];then
+/tmp/updateflag $MASTER $XCATIPORT "installstatus netbooting"
+fi
