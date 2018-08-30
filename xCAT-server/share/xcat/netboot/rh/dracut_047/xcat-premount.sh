092c045d9ac2d491051063d3eb6e91a6749a4c14
Gᴏɴɢ Jie <gongjie@linux.vnet.ibm.com>
2018-08-30 10:29:15 +0800

    Provide RHEL7->RHEL8 diskless support (#5567)
    * Package lists and post-install scripts for RHEL8 diskless support
    
    * RHEL8 kernel finding hack
    
    * Subdirectory dracut_047 for RHEL8 support
    
    * Use /bin/bash for all the scripts
    
    * Use "set -x" for debugging
    
    * Tweak dracut.conf
    
    * Remove dracut-install

---
+++ xCAT-server/share/xcat/netboot/rh/dracut_047/xcat-premount.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+#script to update nodelist.nodestatus during provision
+
+XCAT="$(getarg XCAT=)"
+STATEMNT="$(getarg STATEMNT=)"
+MASTER=`echo $XCAT |awk -F: '{print $1}'`
+
+getarg nonodestatus
+NODESTATUS=$?
+
+XCATIPORT="$(getarg XCATIPORT=)"
+if [ $? -ne 0 ]; then
+XCATIPORT="3002"
+fi
+
+
+
+if [ $NODESTATUS -ne 0 ];then
+/tmp/updateflag $MASTER $XCATIPORT "installstatus netbooting"
+fi
