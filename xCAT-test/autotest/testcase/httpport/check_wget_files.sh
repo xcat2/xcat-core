b4c6425ee374a1991e8c1ade5b1a266278f45e57
litingt <litingt@cn.ibm.com>
2018-11-18 22:02:42 -0500

    update according to comments

--- xCAT-test/autotest/testcase/httpport/check_wget_files.sh
+++ xCAT-test/autotest/testcase/httpport/check_wget_files.sh
@@ -6,20 +6,20 @@ files=$(ls $path)
 for filename in $files
 do
  echo "The file is $filename"
- grep 8898 $path$filename
+ grep $2 $path$filename
  if [ $? != 0 ];then
-    echo "port 8898 does not exist"
+    echo "port $2 does not exist"
     exit 1;
  fi
- links=$(grep 8898 $path$filename|awk -F " " '{print $2}') 
+ links=$(grep $2 $path$filename|awk -F " " '{print $2}') 
  for link in $links
  do 
   wget $link
   if [ $? != 0 ];then
-    echo "wget failed"
+    echo "wget file $link failed"
     exit 1;
   else
-    echo "wget $link succeed"
+    echo "wget file $link succeed"
   fi
  done  
 done
