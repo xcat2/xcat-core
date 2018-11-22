10a858cf61fd65b8a70f15d49550e3cd0dbf1378
xuweibj <xwbj@cn.ibm.com>
2018-11-21 19:59:04 -0500

    fix issue 5719, make output of commands against openbmc same

--- xCAT-server/lib/xcat/plugins/openbmc2.pm
+++ xCAT-server/lib/xcat/plugins/openbmc2.pm
@@ -202,7 +202,7 @@ sub parse_args {
 
     if ($command eq "rbeacon") {
         unless ($subcommand =~ /^on$|^off$|^stat$/) {
-            return ([ 1, "Only 'on', 'off' or 'stat' is supported for OpenBMC managed nodes."]);
+            return ([ 1, "Only 'on', 'off' and 'stat' are supported for OpenBMC managed nodes."]);
         }
     } elsif ($command eq "rflash") {
         my ($activate, $check, $delete, $directory, $list, $upload) = (0) x 6;
